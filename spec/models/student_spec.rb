10be149a608b8961b5df52bc1c69cc171c7fdcfe
Alex Schwartz <aschwartz@detaso.com>
2021-02-15 09:46:40 -0600

    Add models

---
+++ spec/models/student_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Student, type: :model do
+  describe 'relationships' do
+    it { should have_many :professor_students }
+    it { should have_many(:professors).through(:professor_students) }
+  end
+end
