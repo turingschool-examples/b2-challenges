2981417878edb516688cd898a044f3e2b02b5547
Katy La Tour <klatour324@gmail.com>
2021-02-15 13:04:33 -0600

    Create res of spec for professor and complete up to the edit form

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -11,12 +11,25 @@ RSpec.describe "Professor index page" do
     @professor3 = Professor.create!(name: "Professor Orf", age: 30, specialty: "Levitatin")
   end
 
-  it "can see the name of each professor" do
+  it "can see the name of each professor and a link to update a professors info" do
     visit "/professors"
 
     expect(page).to have_content("All Professors")
     expect(page).to have_content(@professor1.name)
     expect(page).to have_content(@professor2.name)
     expect(page).to have_content(@professor3.name)
+    expect(page).to have_link("Update")
+
+    click_link("Update")
+
+    expect(page).to have_content("Update Professor")
+
+    fill_in("name", with: "Professor Wizard")
+    fill_in("age", with: 20)
+    fill_in("specialty", "Casting Spells")
+
+    click_button("Submit")
+
+    expect(current_path).to eq("/professors")
   end
 end
