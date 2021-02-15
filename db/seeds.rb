1069d77edaaf2793ea49de7e6fc70c559a3d427e
Angel Breaux <abreaux@detaso.com>
2021-02-15 10:11:42 -0600

    Create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@professor = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
+@student1 = @professor.students.create!(name: 'Angel', age: 61)
+@student2 = @professor.students.create!(name: 'Chris', age: 86)
