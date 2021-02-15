b4ef880e7135d4a05fd50576f89e2066c791f68e
Katy La Tour <klatour324@gmail.com>
2021-02-15 11:28:37 -0600

    Add seeds file for user_story_1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.destroy_all
+Professor.destroy_all
+
+professor1 = Professor.create!(name: "Professor Snape", age: 65, specialty: "Magic")
+professor2 = Professor.create!(name: "Professor Minerva McGonagall", age: 204, specialty: "Transfiguration")
+professor3 = Professor.create!(name: "Professor Orf", age: 30, specialty: "Levitating")
+
+student1 = professor1.students.create!(name: "Harry", age: 17)
+student2 = professor1.students.create!(name: "Ron", age: 15)
+student3 = professor3.students.create!(name: "Herminie", age: 14)
