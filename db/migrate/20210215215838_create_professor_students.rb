40aa52352ee42bff1f7844307f28712af354fc8f
Arika Turing <ablenker@gmail.com>
2021-02-15 15:00:40 -0700

    Create professor students migration

---
+++ db/migrate/20210215215838_create_professor_students.rb
@@ -0,0 +1,9 @@
+class CreateProfessorStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :professor_students do |t|
+      t.references :professor
+      t.references :student
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_214710) do
+ActiveRecord::Schema.define(version: 2021_02_15_215838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "professor_students", force: :cascade do |t|
+    t.bigint "professor_id"
+    t.bigint "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["professor_id"], name: "index_professor_students_on_professor_id"
+    t.index ["student_id"], name: "index_professor_students_on_student_id"
+  end
 
   create_table "professors", force: :cascade do |t|
     t.string "name"
