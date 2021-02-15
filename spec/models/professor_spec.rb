2a0172821e6cbbe56a5e45aab5de9811f39bcc82
Angel Breaux <abreaux@detaso.com>
2021-02-15 09:52:31 -0600

    Create model specs for professor, student, and professor_student

---
+++ spec/models/professor_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Professor do
+  describe 'relationships' do
+    it { should have_many :professor_students }
+    it { should have_many(:students).through(:professor_students) }
+  end
+end
