24ca61ca73efd66a65506dd321c542840fa8d592
Katy La Tour <klatour324@gmail.com>
2021-02-15 12:29:14 -0600

    Create instance method for professor model to calculate avg age of students per professor

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Professor, type: :model do
     it { should have_many :professor_students }
     it { should have_many(:students).through(:professor_students) }
   end
+
+  describe "instance methods" do
+    describe "#average_age_of_students" do
+      it "can find average of students ages for a professor" do
+        professor = Professor.create!(name: "Professor Snape", age: 65, specialty: "Magic")
+        student1 = Student.create!(name: "Harry", age: 17)
+        student2 = Student.create!(name: "Ron", age: 15)
+        professor_student1 = ProfessorStudent.create!(professor_id: professor.id, student_id: student1.id)
+        professor_student2 = ProfessorStudent.create!(professor_id: professor.id, student_id: student2.id)
+
+        expect(professor.average_age_of_students).to eq(16)
+      end
+    end
+  end
 end
