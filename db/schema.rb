e55906c179f805a461b69d74a3f588bb2b24f370
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 11:31:35 -0700

    Update schema with student table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_180432) do
+ActiveRecord::Schema.define(version: 2021_02_15_181315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2021_02_15_180432) do
     t.string "specialty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
   end
 
 end
