a70ba0b540d29382998860b4c9c29cdb88de8def
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 13:04:28 -0700

    Add edit route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get '/professors', to: 'professors#index'
   get '/professors/:id', to: 'professors#show'
+  get '/professors/:id/edit', to: 'professors#edit'
+  # patch '/professors/:id', to: 'professors#update'
 
   get '/students', to: 'students#index'
 end
