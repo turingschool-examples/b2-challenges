0f0bc6ea9c5f80e6238386db6575af7ad8e69279
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 11:50:23 -0700

    Add stupro join table migration and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_181315) do
+ActiveRecord::Schema.define(version: 2021_02_15_184719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,20 @@ ActiveRecord::Schema.define(version: 2021_02_15_181315) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "student_professors", force: :cascade do |t|
+    t.bigint "professor_id"
+    t.bigint "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["professor_id"], name: "index_student_professors_on_professor_id"
+    t.index ["student_id"], name: "index_student_professors_on_student_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
   end
 
+  add_foreign_key "student_professors", "professors"
+  add_foreign_key "student_professors", "students"
 end
