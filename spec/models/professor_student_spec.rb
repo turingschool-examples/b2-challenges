252e231a8dd10ec735dfc6fca5e8dccd2bc8f2da
Angel Breaux <abreaux@detaso.com>
2021-02-15 11:46:04 -0600

    Create spec to find by student id active record

--- spec/models/professor_student_spec.rb
+++ spec/models/professor_student_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe ProfessorStudent do
     it { should belong_to :professor }
     it { should belong_to :student  }
   end
+
+  describe 'class_methods' do
+    it 'returns record that matches a student id' do
+      @professor1 = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
+      @student1 = Student.create!(id: 1, name: 'Angel', age: 61)
+      professor_student = ProfessorStudent.create!(professor: @professor1, student: @student1)
+
+      expect(ProfessorStudent.find_student_by(@student1.id)).to eq([professor_student])
+    end
+  end
 end
