dee26e3054650710706fe34acda9b2e0e3f67dc5
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 11:52:03 -0700

    Add relationships test to spec

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Professor do
   describe 'relationships' do
     it { should have_many :student_professors }
+    it { should have_many(:students).through(:student_professors)}
   end
 end
