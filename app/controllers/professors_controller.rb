e6b05c70b588950c6851bc7ed805c04a5510ef39
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 13:05:32 -0700

    Add edit action with fetcher

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -7,6 +7,10 @@ class ProfessorsController < ApplicationController
     @professor = Professor.find(params[:id])
   end
 
+  def edit
+    @professor = Professor.find(params[:id])
+  end
+
   private
   def professor_params
     # params.require(:)
