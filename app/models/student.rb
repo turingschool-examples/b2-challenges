3cf5452d6aedb89c389600af74bbb7ba432eca2e
Angel Breaux <abreaux@detaso.com>
2021-02-15 10:23:08 -0600

    Create student average age method

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,9 @@
 class Student < ApplicationRecord
+  validates_presence_of :name, :age
   has_many :professor_students
   has_many :professors, through: :professor_students
+
+  def self.average_age
+    average(:age)
+  end
 end
