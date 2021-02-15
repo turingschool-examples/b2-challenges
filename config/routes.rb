def265f78a0dbc7d25959ad031fa2e4ef1a28eff
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 13:22:56 -0700

    Add patch update route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/professors', to: 'professors#index'
   get '/professors/:id', to: 'professors#show'
   get '/professors/:id/edit', to: 'professors#edit'
-  # patch '/professors/:id', to: 'professors#update'
+  patch '/professors/:id', to: 'professors#update'
 
   get '/students', to: 'students#index'
 end
