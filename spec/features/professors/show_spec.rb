601a7dbbd1658c084c8fbdb6eba4833df8e646c6
Arika Turing <ablenker@gmail.com>
2021-02-15 15:46:23 -0700

    Create feature and model tests for average age

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe 'Professor show page' do
     @student1 = @professor1.students.create!(name: "Harry",
                                              age: 18)
     @student2 = @professor1.students.create!(name: "Ron",
-                                             age: 17)
+                                             age: 20)
+    @student3 = @professor1.students.create!(name: "Hermione",
+                                             age: 16)
   end
 
   it 'displays professors attributes and associated students' do
@@ -19,6 +21,16 @@ RSpec.describe 'Professor show page' do
     expect(page).to have_content("Professor's specialty: #{@professor1.specialty}")
     @professor1.students.each do |student|
       expect(page).to have_content(student.name)
+    end
+  end
+
+  it 'displays an average age of students' do
+    visit "/professors/#{@professor1.id}"
+
+    expect(page).to have_selector('#statistics')
+
+    within('#statistics') do
+      expect(page).to have_content("Average age: 18 years old")
     end
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
+  before :each do
+    @professor1 = Professor.create!(name: "Minerva McGonagall",
+                                    age: 204,
+                                    specialty: "Transfiguration")
+    @student1 = @professor1.students.create!(name: "Harry",
+                                             age: 18)
+    @student2 = @professor1.students.create!(name: "Ron",
+                                             age: 20)
+    @student3 = @professor1.students.create!(name: "Hermione",
+                                             age: 16)
+  end
+
   describe "relationships" do
     it { should have_many :professor_students}
     it { should have_many(:professors).through(:professor_students)}
+  end
+
+  describe 'class methods' do
+    it 'can find average age of students' do
+      expect(Student.average_age). to eq(18)
+    end
   end
 end
