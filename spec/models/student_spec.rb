67b82cec3765d3b5b2ddae24d60c71a9629765a8
Kris Litman <duguy8@gmail.com>
2021-02-15 11:49:37 -0500

    Added class method test for average age in student.rb. On user story 3

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Student, type: :model do
     it { should have_many :professor_students}
     it { should have_many(:professors).through(:professor_students)}
   end
+
+  describe "class methods" do
+    it "should average ages" do
+      @student1 = Student.create!(
+        name: "Whacky kid",
+        age: 20
+      )
+      @student2 = Student.create!(
+        name: "Funny one",
+        age: 11
+      )
+      @student3 = Student.create!(
+        name: "Loud kiddo",
+        age: 13
+      )
+      expected = Student.average_age
+      expect(expected.round).to eq(15)
+    end
+  end
 end
