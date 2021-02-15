ab94541c3e74d41a8bad1cbb504c0e3f4d211cfe
Angel Breaux <abreaux@detaso.com>
2021-02-15 10:22:46 -0600

    Update student spec average age test to return a float

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe Student do
 
   describe 'class methods' do
     it 'returns average age' do
-      @student1 = Student.create!(name: 'Angel', age: 61)
-      @student2 = Student.create!(name: 'Chris', age: 86)
+      student1 = Student.create!(name: 'Angel', age: 61)
+      student2 = Student.create!(name: 'Chris', age: 86)
 
-      expected = (@student1.age + @student2.age)/2
+      expected = (student1.age + student2.age)/2.0
       expect(Student.average_age).to eq(expected)
     end
   end
