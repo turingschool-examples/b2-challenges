bdf641c48eec386708f5f40701d1f7d9517231bb
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 12:40:59 -0700

    Add within controls and setup block

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'the professor index page' do
+  before :each do
+    @mine = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
+    @dumb = Professor.create!(name: 'Albus Dumbledore', age: 345, specialty: 'Hubris')
+  end
+
   it 'displays prof attributes' do
-    prof = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
-
     visit '/professors'
+    within("#professor-#{@mine.id}") do
+      expect(page).to have_link('Minerva McGonagall')
+      expect(page).to have_content('Age: 204')
+      expect(page).to have_content('Specialty: Transfiguration')
+    end
+  end
+  it 'has a link to an update form for each' do
 
-    expect(page).to have_link('Minerva McGonagall')
-    expect(page).to have_content('Age: 204')
-    expect(page).to have_content('Specialty: Transfiguration')
   end
 end
