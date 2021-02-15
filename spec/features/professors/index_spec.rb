e9d489aa55af172c5451bc181e57f0255453798d
Arika Turing <ablenker@gmail.com>
2021-02-15 16:17:50 -0700

    Created test to edit professor info

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -15,10 +15,34 @@ RSpec.describe 'Professor index page' do
     @student3 = @professor1.students.create!(name: "Hermione",
                                              age: 16)
   end
+
   it 'displays all professors' do
     visit '/professors'
 
     expect(page).to have_content(@professor1.name)
     expect(page).to have_content(@professor2.name)
+  end
+
+  it 'includes update button next to each professor' do
+    visit '/professors'
+
+    within "#professor-#{@professor1.id}" do
+      expect(page).to have_link('Update')
+    end
+  end
+
+  it 'can follow link to update professor info' do
+    visit '/professors'
+
+    within "#professor-#{@professor1.id}" do
+      click_on('Update')
+    end
+
+    fill_in "name", with: "Gilderoy Lockhart"
+
+    click_on('Submit')
+
+    expect(current_path).to eq('/professors')
+    expect(page).to have_content("Gilderoy Lockhart")
   end
 end
