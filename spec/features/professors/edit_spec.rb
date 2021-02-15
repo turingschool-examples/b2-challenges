8ac400ac8e8836ef14fd21e8243a4209ee7757f0
Angel Breaux <abreaux@detaso.com>
2021-02-15 11:08:16 -0600

    Update edit spec

--- spec/features/professors/edit_spec.rb
+++ spec/features/professors/edit_spec.rb
@@ -5,16 +5,37 @@ RSpec.describe 'As a visitor' do
     @professor1 = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
   end
 
-  describe 'When I click update link' do
-    it 'I am taken to a form where I can input a new name, age, and specialty' do
+  describe 'When I click the link "Update"' do
+    it 'I am taken to that professors edit page' do
       visit '/professors'
       click_link('Update')
 
       expect(current_path).to eq("/professors/#{@professor1.id}/edit")
     end
+    
+    it 'I am taken to a form where I can input a new name, age, and specialty' do
+      visit '/professors'
+      click_link('Update')
+
+      within("#professor-#{@professor1.id}-edit") do
+        expect(page).to have_content("Name:")
+        expect(page).to have_content("Age:")
+        expect(page).to have_content("Specialty:")
+        expect(page).to have_button('Submit')
+      end
+    end
+
+    it 'I am taken back to the professors index where I see that professor has been updated' do
+      visit '/professors'
+      click_link('Update')
+
+      within("#professor-#{@professor1.id}-edit") do
+        fill_in :name, with: 'New named professor'
+
+        click_button('Submit')
+      end
+      expect(current_path).to eq('/professors')
+      expect(page).to have_content('New named professor')
+    end
   end
 end
-# When I click this link,
-# Then I am taken to a form where I can input a new name, age, and specialty
-# When I click the submit button,
-# Then I am taken back to the professors index where I see that professor has been updated
