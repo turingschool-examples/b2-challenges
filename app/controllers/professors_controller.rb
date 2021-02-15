42ec812d3f0171e2f017f646b48726f782d80145
DougWelchons <doug.welchons@gmail.com>
2021-02-15 10:42:03 -0700

    Add user story 2 test and code

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -2,6 +2,16 @@ class ProfessorsController < ApplicationController
 
   def show
     @professor = Professor.find(params[:id])
-    @students = ProfessorStudent.where(professor_id: "#{params[:id]}")
+    @pairs = ProfessorStudent.where(professor_id: "#{params[:id]}")
+
+    @students = @pairs.map do |pair|
+      Student.find(pair.student_id)
+    end
+
+    total_age = @students.sum do |student|
+      student.age
+    end
+
+    @average_age = (total_age.to_f / @students.count).round(2)
   end
 end
--- spec/features/professor/show_spec.rb
+++ spec/features/professor/show_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe "professors show page" do
     @professor1 = Professor.create!(name: "professor 1", age: 45, specialty: "Magic")
     @professor2 = Professor.create!(name: "professor 2", age: 30, specialty: "Math")
 
-    @student1 = Student.create!(name: "Student 1", age: 15)
-    @student2 = Student.create!(name: "Student 2", age: 16)
-    @student3 = Student.create!(name: "Student 3", age: 17)
-    @student4 = Student.create!(name: "Student 4", age: 18)
+    @student1 = Student.create!(name: "Student 1", age: 10)
+    @student2 = Student.create!(name: "Student 2", age: 12)
+    @student3 = Student.create!(name: "Student 3", age: 14)
+    @student4 = Student.create!(name: "Student 4", age: 16)
 
     @pro1_stu1 = ProfessorStudent.create!(professor: @professor1, student: @student1)
     @pro1_stu3 = ProfessorStudent.create!(professor: @professor1, student: @student3)
@@ -19,6 +19,8 @@ RSpec.describe "professors show page" do
   end
 
   describe "when I visit the professors show page" do
+
+    # User story 1
     it "shows the professors info" do
 
       visit "professors/#{@professor1.id}"
@@ -34,6 +36,15 @@ RSpec.describe "professors show page" do
         expect(page).to_not have_content(@student2.name)
         expect(page).to have_content(@student3.name)
         expect(page).to have_content(@student4.name)
+      end
+    end
+
+    # User story 2
+    it "shows the average age of all the students associated with this professor" do
+      visit "professors/#{@professor1.id}"
+
+      within(".student_info") do
+        expect("avreage student age: 13.33").to appear_before(@student1.name)
       end
     end
   end
