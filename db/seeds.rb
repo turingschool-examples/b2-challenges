85a5734f4ba82c64c50244b760f75c6a5931dab3
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 12:16:34 -0700

    Add some profs and students to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mine = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
+dumb = Professor.create!(name: 'Albus Dumbledore', age: 345, specialty: 'Hubris')
+
+nevi = Student.create!(name: 'Neville Longbottom', age: 17)
+luna = Student.create!(name: 'Luna Lovegood', age: 17)
+cho  = Student.create!(name: 'Cho Chang', age: 18)
