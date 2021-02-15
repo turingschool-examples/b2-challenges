95b72a3528f547eba0ef6ce41f3cd1134c750e1d
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-15 15:15:26 -0700

    Create student and join tables

---
+++ db/migrate/20210215215534_create_professor_students.rb
@@ -0,0 +1,10 @@
+class CreateProfessorStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :professor_students do |t|
+      t.references :student, foreign_key: true
+      t.references :professor, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
