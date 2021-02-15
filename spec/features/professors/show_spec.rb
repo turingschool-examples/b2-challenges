789a1d6d785a6973e426d8514ada391211a646af
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-15 13:46:13 -0700

    Write tests for professor user story 1

---
+++ spec/features/professors/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'As a Visitor' do
+  before :each do
+  	# @application_1 = Application.create!(name: 'Ben', street: '123 street', city: 'Denver', state: 'CO', zip: '80220')
+   #  @shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+   #  @pet1 = @shelter1.pets.create!(image:"", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
+   @professor_1 = Professor.create!(name: "Minerva McGonagall", age: 204, specialty: "Transfiguration")
+  end
+
+  describe'I can visit a professors show page'do
+  	it'displays the information about a professor'do
+  		visit "/professors/#{@professor_1.id}"
+
+  	end
+	end
+
+end
