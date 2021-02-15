833240e5c2835efb370a3718dcc27e579a832083
Alex Schwartz <aschwartz@detaso.com>
2021-02-15 10:11:28 -0600

    Add student list to professor show page

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Professors show page' do
   before :each do
     @mcgonagall = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
+    @mcgonagall.students.create!(name: 'Bobby Hill', age: 15)
+    @mcgonagall.students.create!(name: 'Wesley Crusher', age: 22)
   end
 
   describe 'as a visitor' do
@@ -13,6 +15,15 @@ RSpec.describe 'Professors show page' do
         expect(page).to have_content('Minerva McGonagall')
         expect(page).to have_content('204')
         expect(page).to have_content('Transfiguration')
+      end
+    end
+
+    it 'shows professor student names' do
+      visit "/professors/#{@mcgonagall.id}"
+
+      within('#prof-students') do
+        expect(page).to have_content('Bobby Hill')
+        expect(page).to have_content('Wesley Crusher')
       end
     end
   end
