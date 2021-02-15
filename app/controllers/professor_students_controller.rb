a7eb24ed768036b0878693aa633d4291f0deea40
Andrew Johnston <av.johnst@gmail.com>
2021-02-15 12:45:48 -0700

    Remove unused method from ProfessorStudentController

--- app/controllers/professor_students_controller.rb
+++ app/controllers/professor_students_controller.rb
@@ -1,9 +1,3 @@
 class ProfessorStudentsController < ApplicationController
-  def destroy
-    professor_student = ProfessorStudent.where(professor_id: params[:id])
-    professor = Professor.find(params[:id])
-    # ProfessorStudent.destroy(professor_student.id)
 
-    redirect_to "/professors/#{professor.id}"
-  end
 end
