51719573b55e81b95cec7619b861b0453b731694
Wil McCauley <wmccauley724@gmail.com>
2021-02-15 12:17:41 -0700

    Add first story feature test and pass it

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'the professor show page' do
+  before :each do
+    @mine = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
+    @dumb = Professor.create!(name: 'Albus Dumbledore', age: 345, specialty: 'Hubris')
+    @nevi = Student.create!(name: 'Neville Longbottom', age: 17)
+    @luna = Student.create!(name: 'Luna Lovegood', age: 17)
+    @cho  = Student.create!(name: 'Cho Chang', age: 18)
+
+    @mine.students << @nevi
+    @mine.students << @luna
+    @mine.students << @cho
+
+    @dumb.students << @nevi
+  end
   it 'displays prof attributes' do
-    prof = Professor.create!(name: 'Minerva McGonagall', age: 204, specialty: 'Transfiguration')
+    visit "/professors/#{@mine.id}"
+
+    expect(page).to have_content('Minerva McGonagall')
+    expect(page).to have_content('Age: 204')
+    expect(page).to have_content('Specialty: Transfiguration')
+
+    within('.students') do
+      expect(page).to have_content('Neville Longbottom')
+      expect(page).to have_content('Luna Lovegood')
+      expect(page).to have_content('Cho Chang')
+    end
+
   end
 end
