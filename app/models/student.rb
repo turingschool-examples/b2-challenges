2c79998e4f0f85b7a0ccb7f5ea4af03013dae7c4
Joseph Budina <joebud3@gmail.com>
2021-02-17 10:42:52 -0700

    Add unfinished method

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,9 @@ class Student < ApplicationRecord
   has_many :professor_students
   has_many :professors, through: :professor_students
   validates_presence_of :name, :age
+
+  def self.find_average_age(professor)
+    binding.pry
+    where(professors: professor).avg(:age)
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,11 +13,17 @@ describe Student, type: :model do
   describe 'Class methods' do
     describe ':: it_finds_average_age' do
       it 'finds average age of students' do
-        @student1 = Student.create!(name: "Harry", age: 14)
-        @student2 = Student.create!(name: "Hermoine", age: 13)
-        @student2 = Student.create!(name: "jeff", age: 12)
+        @professor = Professor.create!(name: "Minerva McGonagall", age: 204, specialty: "Transfiguration")
+        @professor = Professor.create!(name: "Minerva McGonagall", age: 204, specialty: "Transfiguration")
+        @professor = Professor.create!(name: "Minerva McGonagall", age: 204, specialty: "Transfiguration")
+        @student1 = @professor.students.create!(name: "Harry", age: 14)
+        @student2 = @professor.students.create!(name: "Hermoine", age: 13)
+        @student3 = @professor.students.create!(name: "jeff", age: 12)
+        @harry = ProfessorStudent.create!(professor: @professor, student: @student1)
+        @hermoine = ProfessorStudent.create!(professor: @professor, student: @student2)
+        @jeff = ProfessorStudent.create!(professor: @professor, student: @student3)
 
-        expect(Student.find_average_age).to eq(13)
+        expect(Student.find_average_age(@professor)).to eq(13)
       end
     end
   end
