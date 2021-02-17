90a4647594fe8e8bd2c6fc81dce45caf8fd23757
Joseph Budina <joebud3@gmail.com>
2021-02-17 09:42:09 -0700

    Update test

--- spec/features/professor/show_spec.rb
+++ spec/features/professor/show_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 describe 'Professor show page' do
-  before each: do 
-    @student1 = Student.create!(name: "Harry" age: 17)
-    @student2 = Student.create!(name: "Hermoine" age: 17)
-    @professor = Professor.create!(Name: "Minerva McGonagall", Age: 204, Specialty: "Transfiguration")
-    @professor_students = ProfessorStudent.create!(professor: @professor, student: @student1, @student2)
+  before :each do 
+    @professor = Professor.create!(name: "Minerva McGonagall", age: 204, specialty: "Transfiguration")
+    @student1 = Student.create!(name: "Harry", age: 17)
+    @student2 = Student.create!(name: "Hermoine", age: 17)
+    @harry = @student1.professor_students.create!(professor: @professor, student: @student1)
+    @hermoine = @student2.professor_students.create!(professor: @professor, student: @student2)
   end
+
   it 'has professors attributes and students associated' do
-    visit '/professors/#{@professor.id}'
+    visit "/professors/#{@professor.id}"
 
     expect(page).to have_content("Minerva McGonagall")
     expect(page).to have_content("204")
@@ -17,7 +19,7 @@ describe 'Professor show page' do
   end
 
   it 'shows average age of students' do
-    visit '/professors/#{@professor.id}'
+    visit "/professors/#{@professor.id}"
 
     expect(page).to have_content("17")
   end
