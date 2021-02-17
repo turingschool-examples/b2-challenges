77a68c7bd12534418c0b5ed54e7fc453b272752d
Jordan Beck <jordanfbeck@gmail.com>
2021-02-16 19:32:34 -0700

    add passing test and code for user story one

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,3 +1,10 @@
-class ProfessorsController < ApplicationController 
+class ProfessorsController < ApplicationController
+  def show
+    @professor = Professor.find(params[:id])
+    # require "pry"; binding.pry
+  end
 
+  # def index
+  #   @professors = Professor.all
+  # end
 end
--- spec/features/professor/show_spec.rb
+++ spec/features/professor/show_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe 'as a visitor' do
   before :each do
     @professor_1 = Professor.create!(name: "Gandalf", age: 72, specialty: "Potions")
-    @student_1 = @Professor_1.students.create!(name: "Timmy", age: 15)
-    @student_2 = @Professor_1.students.create!(name: "Sara", age: 14)
-    @student_3 = @Professor_1.students.create!(name: "Jenny", age: 13)
+    @professor_2 = Professor.create!(name: "Dumbledore", age: 71, specialty: "Spells")
+    @student_1 = @professor_1.students.create!(name: "Timmy", age: 15)
+    @student_2 = @professor_1.students.create!(name: "Sara", age: 14)
+    @student_3 = @professor_1.students.create!(name: "Jenny", age: 13)
+    @student_4 = @professor_2.students.create!(name: "James", age: 12)
   end
   describe 'when i visit the professor show page' do
     it 'i see that profesors name, age, and specialty' do
@@ -21,6 +23,7 @@ RSpec.describe 'as a visitor' do
       expect(page).to have_content(@student_1.name)
       expect(page).to have_content(@student_2.name)
       expect(page).to have_content(@student_3.name)
+      expect(page).not_to have_content(@student_4.name)
     end
   end
 end
