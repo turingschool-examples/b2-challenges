c793307fc4ec412e756fad53c5e83e87b098160c
Jordan Beck <jordanfbeck@gmail.com>
2021-02-16 20:07:22 -0700

    Add passing test and code for user story two

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/professors/:id", to: "professors#show"
 
-
   get "/students", to: "students#index"
 end
