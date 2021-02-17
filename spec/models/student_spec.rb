efea8510a03c337be21fb66ac6d9a5849499d61d
Joseph Budina <joebud3@gmail.com>
2021-02-17 09:48:53 -0700

    Add average age method

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -9,4 +9,16 @@ describe Student, type: :model do
     it { should have_many :professor_students}
     it { should have_many(:professors).through(:professor_students) }
   end
+
+  describe 'Class methods' do
+    describe ':: it_finds_average_age' do
+      it 'finds average age of students' do
+        @student1 = Student.create!(name: "Harry", age: 14)
+        @student2 = Student.create!(name: "Hermoine", age: 13)
+        @student2 = Student.create!(name: "jeff", age: 12)
+
+        expect(Student.find_average_age).to eq(13)
+      end
+    end
+  end
 end
