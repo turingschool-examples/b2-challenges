ad07e20eebfda631993d3002fc876e8bc260d6f8
Dominic Padula <domopadula2192@gmail.com>
2021-02-17 16:27:20 -0700

    add database and columns

---
+++ db/migrate/20210217231911_create_professors.rb
@@ -0,0 +1,12 @@
+class CreateProfessors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :professors do |t|
+      t.string :name
+      t.integer :age
+      t.string :specialty
+      t.string :specialty
+
+      t.timestamps
+    end
+  end
+end
