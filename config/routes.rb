aae88e74e9d0ac542446e4d7bbb8c56fd4afa250
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-17 20:39:32 -0700

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/professors', to:'professors#index'
+  get '/professors/:id', to:'professors#show', as: :professors_show
+
+  # get "/students", to: "students#index"
+  get "/students/:id", to: "students#show", as: :students_show
+
+
 end
