4034dddf5f20b0f4cfa8a1f8d6db34aeee5ecfc5
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-17 21:59:55 -0700

    Update models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,9 @@
-class Student < ApplicationRecord
-has_many :professor_students
-has_many :professors, through: :professor_students
+class Student <ApplicationRecord
+  has_many :professor_students
+  has_many :professors, through: :professor_students
+  validates_presence_of :name, :age
 
-validates_presence_of :name, :age
+  def self.average_age
+    average(:age)
+  end
 end
