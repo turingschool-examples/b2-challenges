68476de8a68b1f77e9fab577969d35a994ab4de3
Gvieve <gnuebel84@yahoo.com>
2021-02-18 11:47:51 -0700

    Add professors#/update routes, actions and view

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -7,4 +7,21 @@ class ProfessorsController < ApplicationController
     @professor = Professor.find(params[:id])
     @students = @professor.students
   end
+
+  def edit
+    @professor = Professor.find(params[:id])
+  end
+
+  def update
+    @professor = Professor.find(params[:id])
+    @professor.update(professor_params)
+
+    redirect_to "/professors/#{@professor.id}"
+  end
+
+  private
+
+  def professor_params
+    params.permit(:name, :age, :specialty)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/professors', to: 'professors#index'
-  get '/professors/:id', to: 'professors#show'
+  get '/professors/:id', to: 'professors#show', as: :professors_show
+  get '/professors/:id/edit', to: 'professors#edit'
+  patch '/professors/:id', to: 'professors#update'
 end
