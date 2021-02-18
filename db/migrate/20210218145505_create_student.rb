d2e9ddd5bcb79723b3881f7625eac5c06239d3d9
Diana Buffone <diana.pamelaet@gmail.com>
2021-02-18 10:02:12 -0500

    Add student model test and file for joins table

---
+++ db/migrate/20210218145505_create_student.rb
@@ -0,0 +1,8 @@
+class CreateStudent < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :age
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_142849) do
+ActiveRecord::Schema.define(version: 2021_02_18_145505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "professor_students", force: :cascade do |t|
+    t.bigint "student_id"
+    t.bigint "professor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["professor_id"], name: "index_professor_students_on_professor_id"
+    t.index ["student_id"], name: "index_professor_students_on_student_id"
+  end
 
   create_table "professors", force: :cascade do |t|
     t.string "name"
@@ -23,4 +32,11 @@ ActiveRecord::Schema.define(version: 2021_02_18_142849) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+  end
+
+  add_foreign_key "professor_students", "professors"
+  add_foreign_key "professor_students", "students"
 end
