7085837a3d93cd5516c2220228b5cc98fcf4f958
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-17 22:09:04 -0600

    Add test for path to edit page.

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -11,7 +11,24 @@ RSpec.describe "professors index page", type: :feature do
 
         expect(page).to have_content(professor1.name)
         expect(page).to have_content(professor2.name)
+      end
+    end
+    describe "each professor has an edit button" do
+      it "has link for update" do
+        professor1 = Professor.create!(name: "Minerva McGonagall", age: 204, specialty: "Transfiguration")
+        professor2 = Professor.create!(name: "Snape", age: 98, specialty: "Spells")
 
+        visit "/professors"
+
+        expect(page).to have_link("Update")
+      end
+      it "clicking link takes to edit path" do
+        professor1 = Professor.create!(name: "Minerva McGonagall", age: 204, specialty: "Transfiguration")
+
+        visit "/professors"
+
+        click_link "Update"
+        expect(current_path).to eq("/professors/professor1.id/edit")
       end
     end
   end
