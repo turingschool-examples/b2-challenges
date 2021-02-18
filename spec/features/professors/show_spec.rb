aed2bf2364e64e73866effe19ff8307f5f770cd3
Dominic Padula <domopadula2192@gmail.com>
2021-02-17 17:49:17 -0700

    user story 2 with model and features test finished

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Professor index page' do
     @lupin = Professor.create!(name: "Remus Lupin", age: 45, specialty: "Defence Against the Dark Arts")
     @snape = Professor.create!(name: "Severus Snape", age: 45, specialty: "Potions")
     @harry = Student.create!(name: "Harry Potter", age:14)
-    @draco = Student.create!(name: "Draco Malfoy", age:14)
+    @draco = Student.create!(name: "Draco Malfoy", age:18)
     @harry_mcgonagall = ProfessorStudent.create!(professor: @mcgonagall, student: @harry)
     @draco_mcgonagall = ProfessorStudent.create!(professor: @mcgonagall, student: @draco)
   end
@@ -15,7 +15,7 @@ RSpec.describe 'Professor index page' do
 
     visit "/professors"
 
-    click_link(@mcgonagall.name)
+    first(:link, @mcgonagall.name).click
     expect(current_path).to eq("/professors/#{@mcgonagall.id}")
     expect(page). to have_content(@mcgonagall.name)
     expect(page). to have_content(@mcgonagall.age)
@@ -26,5 +26,10 @@ RSpec.describe 'Professor index page' do
       visit "/professors/#{@mcgonagall.id}"
       expect(page).to have_content(@harry.name)
       expect(page).to have_content(@draco.name)
+  end
+
+  it "displays the average student age" do
+    visit "/professors/#{@mcgonagall.id}"
+    expect(page).to have_content("Average Age:16.0")
   end
 end
--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,16 @@ describe Professor, type: :model do
       it {should validate_presence_of :specialty}
     end
   end
+
+  it "average student ages can be gotten" do
+    mcgonagall = Professor.create!(name: "Minerva McGonagall", age: 204, specialty: "Transfiguration")
+    lupin = Professor.create!(name: "Remus Lupin", age: 45, specialty: "Defence Against the Dark Arts")
+    snape = Professor.create!(name: "Severus Snape", age: 45, specialty: "Potions")
+    harry = Student.create!(name: "Harry Potter", age:14)
+    draco = Student.create!(name: "Draco Malfoy", age:18)
+    harry_mcgonagall = ProfessorStudent.create!(professor: mcgonagall, student: harry)
+    draco_mcgonagall = ProfessorStudent.create!(professor: mcgonagall, student: draco)
+    expect(mcgonagall.average_age).to eq(16)
+
+  end
 end
