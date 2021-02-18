9bcfc03ff2731cb1cc4bfb3edcea9d1c0ffbd4e3
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-02-18 09:04:41 -0600

    Resume work, add route for edit option.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/professors/:id", to: "professors#show"
   get "/professors", to: "professors#index"
-  get "/professors/:id/edit", to: "professors#edit"
+  get "/professors/:id/edit", to: "professors#edit", as: "professors_edit"
 end
