02079bd9338299d92592dd4c040134768b1790dd
Diana Buffone <diana.pamelaet@gmail.com>
2021-02-18 10:21:22 -0500

    Add final progress for less than 2hrs

---
+++ db/migrate/20210218144426_create_student.rb
@@ -0,0 +1,10 @@
+class CreateStudent < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -2,19 +2,18 @@
 
   RSpec.describe 'When I visit a Professors show page' do
     it 'displays the professors name, age, and specialty' do
-      visit "/professors/#{@minerva.id}"
 
       @minerva = Professor.create!(
         name: "Minerva McGonagall",
         age: 204,
         specialty: "Transfiguration"
       )
+      visit "/professors/#{@minerva.id}"
 
       expect(page).to have_content("Name: Minerva McGonagall, Age: 204, Specialty: Transfiguration")
     end
 
     it 'displays all the Student names associated with this professor' do
-      visit "/professors/#{@minerva.id}"
 
       @minerva = Professor.create!(
         name: "Minerva McGonagall",
@@ -23,17 +22,16 @@
       )
       @harry = Student.create!(
         name: 'Harry Potter',
-        age: '14',
-        house: 'Hogwarts'
+        age: '14'
       )
       @cass = Student.create!(
         name: 'Casseopia Black',
-        age: '14',
-        house: 'Slytherin'
+        age: '14'
       )
 
       ProfessorStudent.create!(professor: @minerva, student: @cass)
       ProfessorStudent.create!(professor: @minerva, student: @harry)
+      visit "/professors/#{@minerva.id}"
 
       expect(page).to have_content('Harry Potter')
       expect(page).to have_content('Casseopia Black')
