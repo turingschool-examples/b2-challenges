f6ab5f38abff3164173757549364d8f2d3f068fa
joemecha <jwmecha@gmail.com>
2021-04-05 12:13:14 -0500

    Add seeds to confirm results in browser

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Mechanic.destroy_all
+Ride.destroy_all
+
+anna = Mechanic.create!(name: 'Anna Allen', years_experience: 9)
+bradley = Mechanic.create!(name: 'Bradley Driver', years_experience: 10)
+chelsey = Mechanic.create!(name: 'Chelsey Vise', years_experience: 11)
+
+wizzer = Ride.create!(name: 'Wizzer', thrill_rating: 2, ride_open: true)
+bumper_cars = Ride.create!(name: 'Bumper Cars', thrill_rating: 4, ride_open: true)
+zoomy = Ride.create!(name: 'Zoomy Death Plunge', thrill_rating: 10, ride_open: true)
+no_brakes = Ride.create!(name: 'Brakeless Rusty Coaster', thrill_rating: 8, ride_open: false)
