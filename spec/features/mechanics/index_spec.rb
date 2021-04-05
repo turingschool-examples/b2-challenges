5fa556feafa1b33d31687ff3bb8b27402d467cf5
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-05 11:48:22 -0400

    Create feature test for us1 mechanics index page

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'the mechanics index page' do
+  it 'lists all mechanic’s names and their years of experience' do
+    mechanic1 = Mechanic.find_or_create_by!(name: "Klaud", years_experience: 3)
+    mechanic2 = Mechanic.find_or_create_by!(name: "Goliath", years_experience: 24)
+
+    visit '/mechanics'
+
+    expect(page).to have_content("All Mechanics")
+    expect(page).to have_content(mechanic1.name)
+    expect(page).to have_content(mechanic2.name)
+    expect(page).to have_content(mechanic1.years_experience)
+    expect(page).to have_content(mechanic2.years_experience)
+  end
+end
