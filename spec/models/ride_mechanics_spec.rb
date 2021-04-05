c9208b0cc0b55b8451d2e2a2e92a50bcbcc7fc29
kevn7n <kevn_nguyen@yahoo.com>
2021-04-05 09:21:00 -0600

    Initial commit, relationship tests

---
+++ spec/models/ride_mechanics_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe RideMechanics, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:ride) }
+    it { should belong_to(:mechanic) }
+  end
