9f16d672bf22f23eb6dc89323b7ba336ec4a1ad6
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-05 10:37:10 -0600

    Combine AR

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,11 +2,11 @@ class Ride < ApplicationRecord
   has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
 
-  def self.is_open
-    where(open: true)
-  end
+  # def self.is_open
+  #   where(open: true)
+  # end
 
-  def self.sort_by_most_thrills
-    order(rating: :desc)
+  def self.isort_by_most_thrills_and_open
+    where(open: true).order(rating: :desc)
   end
 end
