88b86283910f1f492bfb222179f3a519a409740a
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-05 09:49:36 -0600

    Update schema

---
+++ db/migrate/20210405152936_create_ride.rb
@@ -0,0 +1,11 @@
+class CreateRide < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rides do |t|
+      t.string :name
+      t.integer :rating
+      t.boolean :open
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210405153102_create_mechaninc_rides.rb
@@ -0,0 +1,10 @@
+class CreateMechanincRides < ActiveRecord::Migration[5.2]
+  def change
+    create_table :mechanic_rides do |t|
+      t.references :mechanic, foreign_key: true
+      t.references :ride, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
