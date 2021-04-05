1f68d0f10d01d699cd6cc35a3d6662d9d2177d05
Aidan McGuire <>
2021-04-05 11:34:53 -0600

    Add user story 3

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -9,4 +9,13 @@ class MechanicsController < ApplicationController
     open_rides = @mechanic.mechanics_open_rides
     @rides = open_rides.order_rides_by_thrill
   end
+  
+  def update
+    mechanic = Mechanic.find(params[:id])
+    ride = Ride.find(params[:new_ride])
+    @mechanic = mechanic.rides << ride
+    
+    @all_rides = mechanic.rides
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
 end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -28,4 +28,25 @@ RSpec.describe 'Mechanic show page' do
     expect(page).to have_content("#{ride2.name}")
     expect(page).to_not have_content("#{ride3.name}")
   end
+
+  it "has a form to add a ride to the mechanic's show page" do
+    bob = Mechanic.create!(name: "Bob", experience: 5)
+    ride1 = Ride.create!(name: "Mind Eraser", thrill_rating: 7, open: true)
+    ride2 = Ride.create!(name: "Twister2", thrill_rating: 8, open: true)
+    ride3 = Ride.create!(name: "Loopdy Loop", thrill_rating: 4, open: true)
+
+    bob.rides.push(ride1, ride2)
+    visit "/mechanics/#{bob.id}"
+    expect(page).to have_button("Add Ride")
+    
+    fill_in :new_ride, with: "#{ride3.id}"
+    click_button "Add Ride"
+    
+    expect(current_path).to eq("/mechanics/#{bob.id}")
+    expect(bob.rides).to eq([ride1, ride2, ride3])
+    
+    expect(page).to have_content("#{ride1.name}")
+    expect(page).to have_content("#{ride2.name}")
+    expect(page).to have_content("#{ride3.name}")
+  end
 end
