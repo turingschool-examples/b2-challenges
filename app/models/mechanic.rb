32370c2bb9fefed03d9f173c273b8b111b753d6d
koshea9 <keegan.oshea9@gmail.com>
2021-04-05 10:10:42 -0600

    Add validations

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,6 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
+  validates :name, presence: true
+  validates :years_experience, presence: true, numericality: true
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,7 @@
 class Ride < ApplicationRecord
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
+  validates :name, presence: true
+  validates :thrill_rating, presence: true, numericality: true
+  # validates :open, inclusion: [true, false]
 end
