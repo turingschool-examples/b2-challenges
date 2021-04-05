1a96b4c196093587081ab6342b2249bf6fa5e22b
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-04-05 11:47:30 -0500

    Update show page to show only open rides

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -2,22 +2,24 @@ RSpec.describe "mechanics index page", type: :feature do
   before :each do
     @mechanic_1 = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
     @mechanic_2 = Mechanic.create!(name: 'John Doe', years_experience: 5)
+
     @ride_1 = Ride.create!(name: 'The Hurler', thrill_rating: 7, open: false)
     @ride_2 = Ride.create!(name: 'Free Fall', thrill_rating: 9, open: true)
     @ride_3 = Ride.create!(name: 'Big Splash', thrill_rating: 3, open: true)
+
     RideMechanic.create!(ride: @ride_1, mechanic: @mechanic_1)
     RideMechanic.create!(ride: @ride_2, mechanic: @mechanic_1)
     RideMechanic.create!(ride: @ride_2, mechanic: @mechanic_2)
     RideMechanic.create!(ride: @ride_3, mechanic: @mechanic_2)
   end
 
-  it "can see name and years eaxperience" do
+  it "can see name and years experience" do
     visit "/mechanics/#{@mechanic_1.id}"
 
     expect(page).to have_content(@mechanic_1.name)
     expect(page).to have_content(@mechanic_1.years_experience)
-    expect(page).to have_content(@ride_1.name)
     expect(page).to have_content(@ride_2.name)
+    expect(page).not_to have_content(@ride_1.name)
   end
 
 
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Ride, type: :model do
     it { should have_many :ride_mechanics}
     it {should have_many(:mechanics).through(:ride_mechanics)}
   end
+
+  before :each do
+    @ride_1 = Ride.create!(name: 'The Hurler', thrill_rating: 7, open: false)
+    @ride_2 = Ride.create!(name: 'Free Fall', thrill_rating: 9, open: true)
+    @ride_3 = Ride.create!(name: 'Big Splash', thrill_rating: 3, open: true)
+  end
+
+  describe 'class methods' do
+    describe '#open_rides' do
+      it 'can see only rides that are open' do
+        expect(Ride.open_rides).to eq([@ride_2, @ride_3])
+      end 
+    end
+  end
+
+  describe 'instance methods' do
+  end
 end
