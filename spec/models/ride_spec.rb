4100050cf2901957dd390c72179176582344582b
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-05 11:31:14 -0400

    Create rides model spec and rides model and table

---
+++ spec/models/ride_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Ride, type: :model do
+  describe 'relationships' do
+    it { should have_many :mechanics }
+    it { should have_many :mechanic_rides }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :thrill_rating}
+    it { should validate_presence_of :open}
+  end
+end 
