9be4cebb56f9b4f7f864cbed4a6162b67c869c01
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-04-05 11:36:13 -0600

    tests for user story 2

--- spec/features/mecanics_spec.rb
+++ spec/features/mecanics_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "when i visit mechanics" do
+RSpec.describe "when i visit /mechanics" do
   it "i see all mechanics listed" do
 
     mechanic_1 = Mechanic.create!(name: "andrew", experiance: 1)
@@ -30,6 +30,51 @@ RSpec.describe "when i visit mechanics" do
     visit "/mechanics"
 
     expect(page).to have_content(Mechanic.all.average_exp)
+
+  end
+
+  describe "when i visit /mechanics/:id" do
+    it "i see all mechanic info " do
+
+      mechanic_2 = Mechanic.create!(name: "steven", experiance: 30)
+
+      ride_1 = Ride.create!(name: "ride_1", thrill_rateing: 1, open: true)
+      ride_2 = Ride.create!(name: "ride_2", thrill_rateing: 2, open: true)
+      ride_3 = Ride.create!(name: "ride_3", thrill_rateing: 3, open: false)
+      ride_4 = Ride.create!(name: "ride_4", thrill_rateing: 4, open: true)
+
+
+      mechanic_ride_2b = MechanicRide.create!(ride: ride_2, mechanic: mechanic_2)
+      mechanic_ride_2d = MechanicRide.create!(ride: ride_4, mechanic: mechanic_2)
+      mechanic_ride_2a = MechanicRide.create!(ride: ride_1, mechanic: mechanic_2)
+
+
+      visit "/mechanics/#{mechanic_2.id}"
+
+      expect(page).to have_content(mechanic_2.name)
+      expect(page).to have_content(mechanic_2.experiance)
+
+      expect(page).to have_content(ride_1.name)
+      expect(page).to have_content(ride_2.name)
+      expect(page).to_not have_content(ride_3.name)
+      expect(page).to_not have_content(ride_4.name)
+    end
+
+    # it "i see the rides listed in order of thrill rating"  do
+    #   mechanic_2 = Mechanic.create!(name: "steven", experiance: 30)
+    #
+    #   ride_1 = Ride.create!(name: "ride_1", thrill_rateing: 1, open: true)
+    #   ride_2 = Ride.create!(name: "ride_2", thrill_rateing: 2, open: true)
+    #   ride_3 = Ride.create!(name: "ride_3", thrill_rateing: 3, open: false)
+    #   ride_4 = Ride.create!(name: "ride_4", thrill_rateing: 4, open: true)
+    #
+    #
+    #   mechanic_ride_2b = MechanicRide.create!(ride: ride_2, mechanic: mechanic_2)
+    #   mechanic_ride_2d = MechanicRide.create!(ride: ride_4, mechanic: mechanic_2)
+    #   mechanic_ride_2a = MechanicRide.create!(ride: ride_1, mechanic: mechanic_2)
+    #
+    #   expect(ride_2.name).to appear_before(ride_1.name)
+    # end
 
   end
 
