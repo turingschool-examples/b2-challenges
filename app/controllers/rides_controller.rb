958f515e21b416795b23c671fb44e99591fd998e
Olivia Charles <ocharles92@gmail.com>
2021-04-05 13:18:47 -0400

    trying to get user story 3 to work

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,2 +1,6 @@
 class RidesController < ApplicationController
+
+  def index
+    @rides = Ridel.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
   get '/mechanics/:id', to: 'mechanics#show'
+  get '/mechanics/:id', to: 'mechanics#update'
+
+  post '/mechanics/:id', to: 'mechanic_rides#create'
+
+  get '/rides', to: 'rides#index'
 end
