d36a0c2ef3f30121c7341164f5da7431c2465ce4
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-05 11:59:22 -0400

    Add mechanics controller and index view to complete us1

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,6 +1,10 @@
 class Mechanic < ApplicationRecord
   validates_presence_of :name, :years_experience
-  
+
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
+
+  def self.average_years
+    average(:years_experience)
+  end
 end
--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe 'the mechanics index page' do
     expect(page).to have_content(mechanic1.years_experience)
     expect(page).to have_content(mechanic2.years_experience)
   end
+
+  it 'shows the average years of experience across all mechanics' do
+    mechanic1 = Mechanic.find_or_create_by!(name: "Klaud", years_experience: 30)
+    mechanic2 = Mechanic.find_or_create_by!(name: "Goliath", years_experience: 24)
+
+    visit '/mechanics'
+
+    expect(page).to have_content("Average years of experience: 27")
+  end 
 end
