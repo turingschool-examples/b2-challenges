c53166b89b1dc1b8da729596975fa00200697569
jaharaclark <jaharamclark@gmail.com>
2021-04-05 12:40:02 -0400

    Create show spec test

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSspec.describe 'User Story 2', type: :feature do
+  it 'can display the mechanic show page' do
+    
+  end
+end
+
+# As a user,
+# When I visit a mechanic show page
+# I see their name, years of experience, and the names of all rides they’re working on
+# And I only see rides that are open
+# And the rides are listed by thrill rating in descending order (most thrills first)
