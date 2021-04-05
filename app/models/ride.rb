1c62ab829238259923a1404ac8cfd6d6fa4d5bca
George Ho <george.h.w.ho@gmail.com>
2021-04-05 11:54:31 -0400

    Add thrill rating desc

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,8 @@
 class Ride < ApplicationRecord
   has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
+
+  def self.desc_thrill_rating
+    order(thrill_rating: :DESC)
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Ride, type: :model do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:mechanics) }
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @ride_1 = Ride.create!(name: 'fast', thrill_rating: 5, open: true)
+      @ride_2 = Ride.create!(name: 'medium', thrill_rating: 3, open: false)
+      @ride_3 = Ride.create!(name: 'slow', thrill_rating: 7, open: true)
+    end
+
+    describe '#desc_thrill_rating' do
+      it 'orders the rides by thrill rating' do
+        expect(Ride.desc_thrill_rating).to eq([@ride_3, @ride_1, @ride_2])
+      end
+    end
+  end
 end
