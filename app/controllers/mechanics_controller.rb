6e57eaddb6fce2772b5c3972c26db87b38827e89
Aidan McGuire <>
2021-04-05 10:31:13 -0600

    Add mechanic show page

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -3,4 +3,8 @@ class MechanicsController < ApplicationController
     @mechanics = Mechanic.all
     @average_experience = Mechanic.average_experience
   end
+
+  def show
+    @mechanic = Mechanic.find(params[:id])
+  end
 end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Mechanic show page' do 
-  it "lists all the mechanis name and experience" do
+  it "lists the mechanic's name and experience" do
+    bob = Mechanic.create!(name: "Bob", experience: 5)
+    # ride1 = Ride.create!(name: "Mind Eraser", thrill_rating: 7, open: true)
+    # ride2 = Ride.create!(name: "Twister2", thrill_rating: 8, open: true)
+    # ride3 = Ride.create!(name: "Loopdy Loop", thrill_rating: 4, open: false)
+
+    # bob.rides.push(ride1, ride2, ride3)
+    visit "/mechanics/#{bob.id}"
+    
+    expect(page).to have_content("#{bob.name}")
+    expect(page).to have_content("#{bob.experience}")
+  end
+    
+  it "displays all rides that the mechanic is working on that are open" do
     bob = Mechanic.create!(name: "Bob", experience: 5)
     ride1 = Ride.create!(name: "Mind Eraser", thrill_rating: 7, open: true)
     ride2 = Ride.create!(name: "Twister2", thrill_rating: 8, open: true)
@@ -9,23 +22,10 @@ RSpec.describe 'Mechanic show page' do
 
     bob.rides.push(ride1, ride2, ride3)
 
-    visit '/mechanics/:id'
+    visit "/mechanics/#{bob.id}"
     
-    expect(page).to have_content("#{bob.name}")
-    expect(page).to have_content("#{bob.experience}")
-    
-    it "displays all rides that the mechanic is working on that are open" do
-      bob = Mechanic.create!(name: "Bob", experience: 5)
-      ride1 = Ride.create!(name: "Mind Eraser", thrill_rating: 7, open: true)
-      ride2 = Ride.create!(name: "Twister2", thrill_rating: 8, open: true)
-      ride3 = Ride.create!(name: "Loopdy Loop", thrill_rating: 4, open: false)
-  
-      bob.rides.push(ride1, ride2, ride3)
-  
-      visit '/mechanics/:id'
-      
-      expect(page).to have_content("#{ride1}")
-      expect(page).to have_content("#{ride2}")
-    end
+    expect(page).to have_content("#{ride1.name}")
+    expect(page).to have_content("#{ride2.name}")
+    expect(page).to_not have_content("#{ride3.name}")
   end
 end
