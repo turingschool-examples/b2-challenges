0d468175640ee2a181c882bcb7701b1cb81c36c4
ignored-comment <info.konham@gmail.com>
2021-04-05 12:02:35 -0400

    Create mechanic model test for average years experience; assertions fail

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many :ride_mechanics}
     it { should have_many(:rides).through(:ride_mechanics)}
   end
+
+  it 'can return an average of all the years of experience for mechanics' do
+    mech1 = Mechanic.create!(
+      name: "Bob",
+      years_experience: 12
+    )
+    mech1 = Mechanic.create!(
+      name: "Bobette",
+      years_experience: 15
+    )
+
+    expect(Mechanic.average_years_experience).to eq(13.5)
+  end
 end
