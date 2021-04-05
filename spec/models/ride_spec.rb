09206555c157c7ef379aabca4b148415c3f4b31f
joemecha <jwmecha@gmail.com>
2021-04-05 10:54:42 -0500

    Ride model spec and model

---
+++ spec/models/ride_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Ride, type: :model do
+  before(:each) do
+    @wizzer = Ride.create!(name: 'Wizzer', thrill_rating: 4)
+    @bumper_cars = Ride.create!(name: 'Bumper Cars', thrill_rating: 2)
+    @zoomy = Ride.create!(name: 'Zoomy Death Plunge', thrill_rating: 10)
+  end
+
+  describe 'relationships' do
+    it {should have_many :mechanic_rides}
+    it {should have_many(:mechanics).through(:mechanic_rides)}
+  end
+
+  describe 'class methods' do
+    describe '#' do
+      # it '' do
+        # expect().to eq()
+      # end
+    end
+  end
+end
