8d8543befb76c2315029aabbf8821b8518e180ba
George Ho <george.h.w.ho@gmail.com>
2021-04-05 11:32:13 -0400

    Add mechanic index page

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'the mechanics index page' do
+  it 'lists all of the mechanics name and their years of experience' do
+    mech_1 = Mechanic.create!(name: 'bob', years_experience: '12')
+    mech_2 = Mechanic.create!(name: 'dod', years_experience: '21')
+
+    visit "/mechanics"
+
+    expect(page).to have_content("All Mechanics")
+    expect(page).to have_content(mech_1.name)
+    expect(page).to have_content(mech_1.years_experience)
+
+    expect(page).to have_content(mech_2.name)
+    expect(page).to have_content(mech_2.years_experience)
+  end
+end
