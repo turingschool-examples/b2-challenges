68c0fdffbc0abde38d88b94dc6912470646bb00f
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-05 10:17:04 -0700

    spelling error

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe "When I visit mechanics show page" do
   before :each do
     @mechanic_1 = Mechanic.create!(name: 'Olafur', years_experience: 5)
-    @ride_1 = @mechanic_1.rides.create!(name: "Anacondo", thrill_rating: 6, open: true)
+    @ride_1 = @mechanic_1.rides.create!(name: "Anaconda", thrill_rating: 6, open: true)
     @ride_2 = @mechanic_1.rides.create!(name: "Superman", thrill_rating: 7, open: true)
     @ride_3 = @mechanic_1.rides.create!(name: "Millenium Force", thrill_rating: 10, open: true)
   end   
