d594ae1a99a978933823340ec8d131410443e711
ignored-comment <info.konham@gmail.com>
2021-04-05 12:07:37 -0400

    Complete user story 1

--- spec/features/mechanic/index_spec.rb
+++ spec/features/mechanic/index_spec.rb
@@ -19,5 +19,20 @@ RSpec.describe "Mechanics index page", type: :feature do
       expect(page).to have_content("12")
       expect(page).to have_content("15")
     end
+
+    it 'I can see an average of all the years of experience for mechanics' do
+      mech1 = Mechanic.create!(
+        name: "Bob",
+        years_experience: 12
+      )
+      mech1 = Mechanic.create!(
+        name: "Bobette",
+        years_experience: 15
+      )
+
+      visit 'mechanics'
+
+      expect(page).to have_content("Average years of experience for all mechanics: 13.5")
+    end
   end
 end
