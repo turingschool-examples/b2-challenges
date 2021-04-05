492aabbf30fbc49b23c597e0ad165672aa32c6e9
ignored-comment <info.konham@gmail.com>
2021-04-05 12:30:08 -0400

    Create ride model test order_desc_by_thrill_rating; assertions fail

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe Ride, type: :model do
     it { should have_many :ride_mechanics}
     it { should have_many(:mechanics).through(:ride_mechanics)}
   end
+
+  it 'can order rides by :thrill_rating' do
+    ride1 = Ride.create!(
+      name: "Holy Terror of Babel",
+      thrill_rating: 8,
+      open: true
+    )
+    ride2 = Ride.create!(
+      name: "REGRETABLE9000",
+      thrill_rating: 2,
+      open: true
+    )
+    ride3 = Ride.create!(
+      name: "No More Limbs",
+      thrill_rating: 4,
+      open: true
+    )
+    ride4 = Ride.create!(
+      name: "Last Will and Testament",
+      thrill_rating: 3,
+      open: false
+    )
+    expect(Ride.order_desc_by_thrill_rating).to eq([ride1, ride3, ride4, ride2])
+  end
 end
