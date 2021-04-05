5183887702523758036c2c3f27d585917f4d62c2
Olivia Charles <ocharles92@gmail.com>
2021-04-05 11:56:22 -0400

    User story 1 passing, all mechanics, with attributes and average years of experience

---
+++ app/controllers/mechanics_rides_controller.rb
@@ -0,0 +1,2 @@
+class MechanicRidesController < ApplicationController
+end
--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,22 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Mechanics Index'do
   before(:each) do
-    @jane = Mechanic.create(name: "Jane G", years_experience: 12)
-    @sherry = Mechanic.create(name: "Sherry S", years_experience: 28)
-    @ash = Mechanic.create(name: "Ashley M", years_experience: 4)
+    @jane = Mechanic.create(name: "Jane G", years_experience: 2)
+    @sherry = Mechanic.create(name: "Sherry S", years_experience: 12)
+    @ash = Mechanic.create(name: "Ashley M", years_experience: 10)
   end
 
   it'lists a mechanic with their name and attributes and I can see the average years of exeprience' do
     visit '/mechanics'
 
-    exepct(page).to have_content("All Mechanics")
+    expect(page).to have_content("All Mechanics")
     expect(page).to have_content(@jane.name)
     expect(page).to have_content(@jane.years_experience)
     expect(page).to have_content(@sherry.name)
     expect(page).to have_content(@sherry.years_experience)
     expect(page).to have_content(@ash.name)
     expect(page).to have_content(@ash.years_experience)
-    expect(page).to have_content("Average age of Mechanics: 14.7")
+    expect(page).to have_content("Average years of experience: 8")
+
   end
 end
 
