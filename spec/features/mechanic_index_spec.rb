fbcb80f5cdc727957ce05fcf9fcfd55702b38737
Joe Ray <jray100.jr@gmail.com>
2021-04-05 11:04:26 -0600

    Add features test for mechanic index page

---
+++ spec/features/mechanic_index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "mechanics index page", type: :feature do
+  before :each do
+    steve = Mechanic.create!(name: 'Steve', years_experience: 4)
+    bill = Mechanic.create!(name: 'Bill', years_experience: 6)
+  end
+
+  describe "mechanics index page" do
+    visit '/mechanics'
+
+    expect(page).to have_content(steve.name)
+    expect(page).to have_content(steve.years_experience)
+  end
+end
