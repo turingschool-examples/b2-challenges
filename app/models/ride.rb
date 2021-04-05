ca26db5e75a444d702970983244813175d602af0
joemecha <jwmecha@gmail.com>
2021-04-05 11:42:07 -0500

    Add class method test, method 'open_rides'

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,8 @@
 class Ride < ApplicationRecord
    has_many :mechanic_rides
    has_many :mechanics, through: :mechanic_rides
+
+  def self.open_rides
+    where(ride_open: true)
+  end 
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 RSpec.describe Ride, type: :model do
   before(:each) do
     @wizzer = Ride.create!(name: 'Wizzer', thrill_rating: 2, ride_open: true)
-    @bumper_cars = Ride.create!(name: 'Bumper Cars', thrill_rating: 4, ride_open: true)
+    @bumper_cars = Ride.create!(name: 'Bumper Cars', thrill_rating: 4, ride_open: false)
     @zoomy = Ride.create!(name: 'Zoomy Death Plunge', thrill_rating: 10, ride_open: true)
   end
 
   describe 'relationships' do
     it {should have_many :mechanic_rides}
     it {should have_many(:mechanics).through(:mechanic_rides)}
+  end
+
+  describe 'class methods' do
+    describe '::open_rides' do
+      it 'only shows ride if ride_open is true' do
+        expect(Ride.open_rides.to_a).to eq([@wizzer, @zoomy])
+      end
+    end
   end
 end
