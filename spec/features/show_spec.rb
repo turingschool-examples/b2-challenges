d9ad174c27b5e7f5a9d64551cc9300cc3e35e82c
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-24 10:19:13 -0600

    Add expected feature tests for show page

---
+++ spec/features/show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'The show page for mechanics,' do
+
+  before :all do
+  end
+
+  before :each do
+  end
+
+  after :all do
+  end
+
+  it 'has a header with the name of the mechanic'
+
+  it 'shows years of experience'
+
+  describe 'current rides they are working on,' do
+    it 'has a header'
+
+    it 'shows rides this mechanic is working on'
+
+    it 'displays list of rides in descending order by thrill rating'
+
+    it 'only displays rides that are open'
+  end
+end
