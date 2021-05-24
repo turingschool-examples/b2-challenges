dcccddcd31996a603eb35a12e513a0bf23875790
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-05-24 10:46:58 -0600

    passed 3rd user story

--- app/controllers/ride_mechanics_controller.rb
+++ app/controllers/ride_mechanics_controller.rb
@@ -1,5 +1,9 @@
 class RideMechanicsController < ApplicationController
   def new
-    
+    @mech_id = params[:mech_id]
+  end
+  def create
+    work = RideMechanic.create(ride_id:params[:rideid], mechanic_id: params[:mech_id])
+    redirect_to "/mechanics/#{params[:mech_id]}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :mechanics
-  get "/mechanics/:id/ride_mechanics/new", to: "ride_mechanics#new"
+  resources :mechanics, :ride_mechanics
+  get "/mechanics/:mech_id/ride_mechanics/new", to: "ride_mechanics#new"
 end
