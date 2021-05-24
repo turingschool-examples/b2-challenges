65ea64bceb52b243a33e361b438f94ddfbcdb101
JoannaCoPo <jococopo@gmail.com>
2021-05-24 12:27:05 -0600

    End on story 2, ride working on

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -5,5 +5,13 @@ class MechanicsController < ApplicationController
 
   def show
     @mechanic = Mechanic.find(params[:id])
+    @mechanics = Mechanic.all
+  # if params[:open].open?
   end
 end
+
+if params[:search].present?
+      @pets = Pet.search(params[:search])
+    else
+      @pets = Pet.adoptable
+    end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe 'the mechanics show page', type: :feature do
   before(:each) do
     @mechanic_1 = Mechanic.create!(name: 'Kara Smith', experience: 11)
-    @mechanic_2 = Mechanic.create!(name: 'Jo Olson', experience: 4)
     @mechanic_3 = Mechanic.create!(name: 'Letty Ortiz', experience: 9)
+    @ride_2 = Ride.create!(name:'Mind Eraser', rating: 8, open: true)
   end
 
   it 'shows a mechanics attributes the names of all rides they’re working on' do
@@ -14,14 +14,29 @@ RSpec.describe 'the mechanics show page', type: :feature do
     expect(page).to have_content(@mechanic_1.experience)
 
     visit "/mechanics/#{@mechanic_3.id}"
-    save_and_open_page
+
     expect(page).to have_content(@mechanic_3.name)
     expect(page).to have_content(@mechanic_3.experience)
   end
+
+  it 'displays the names of all rides a mechanic is working on' do
+    m2 = @ride_2.mechanics.create!(name: 'Jo Olson', experience: 4)
+
+
+    visit "/mechanics/#{m2.id}"
+
+    expect(page).to have_content(@mechanic_1.name)
+    expect(page).to have_content(@mechanic_1.experience)
+    expect(page).to have_content('Currently working on: Mind Eraser')
+  end
 end
 
-# As a user,
-# When I visit a mechanic show page
-# I see their name, years of experience, and the names of all rides they’re working on
-# And I only see rides that are open
-# And the rides are listed by thrill rating in descending order (most thrills first)
+#for open rides:
+ # if params[:open].open?
+  #do stuff
+#else
+  #do stuff
+
+#def open?
+    #where(open: true)
+#end
