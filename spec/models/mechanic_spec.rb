683c6e09eb2655f83f3f4cb8e83e0d809cf52365
Suzie K <suziekim.dev@gmail.com>
2021-05-24 11:50:14 -0400

    Red test ::exp_in_years_avearge method

---
+++ spec/models/mechanic_spec.rb
@@ -0,0 +1,13 @@
+#spec/models/mechanic_spec
+require 'rails_helper'
+
+RSpec.describe Mechanic, type: :model do
+  describe 'class methods' do
+    it '::exp_in_years_average' do
+      mechanic_1 = Mechanic.create!(name: "Ryan", exp_in_years: 11)
+      mechanic_2 = Mechanic.create!(name: "Bryan", exp_in_years: 5)
+
+      expect(Mechanic.exp_in_years_average).to eq(8)
+    end
+  end
+end
