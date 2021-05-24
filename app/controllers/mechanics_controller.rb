7b8712f2f76055a1f20e1c789e1936882553abcd
Alex Ferencz <alex.ferencz6@gmail.com>
2021-05-24 09:45:40 -0600

    Create basic file structure

---
+++ app/controllers/mechanics_controller.rb
@@ -0,0 +1,9 @@
+class MechanicsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mechanic1 = Mechanic.create!(name: 'Kara', years_experience: 11)
+mechanic2 = Mechanic.create!(name: 'James', years_experience: 5)
+
+
+ride1 = Ride.create!(name: 'Scrambler', thrill_rating: 5, open: false)
+ride2 = Ride.create!(name: 'Nitro', thrill_rating: 10, open: false)
+ride3 = Ride.create!(name: 'Zipper', thrill_rating: 3, open: true)
+ride4 = Ride.create!(name: 'El Toro', thrill_rating: 8, open: true)
