55872a295942db351eb6579dcc63831b9870e2cd
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-24 10:48:10 -0600

    Add models to join tables

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,6 +1,12 @@
 class Mechanic < ApplicationRecord
+  has_many :mechanic_rides
+  has_many :rides, through: :mechanic_rides
 
   def self.avg_years_experience
     average(:years_experience)
+  end
+
+  def thrill_rating_order_on_open_rides
+    rides.order(thrill_rating: :desc).where(open: true)
   end
 end
