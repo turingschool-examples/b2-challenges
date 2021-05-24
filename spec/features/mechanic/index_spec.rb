f095291ded4c19e8053aa76c6fc4769951a6801b
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-24 10:00:12 -0600

    Finish user story 1

--- spec/features/mechanic/index_spec.rb
+++ spec/features/mechanic/index_spec.rb
@@ -3,13 +3,24 @@ require 'rails_helper'
 RSpec.describe Mechanic do
   describe 'index page' do
     it 'lists all mechanics & their attributes' do
-      mechanic_1 = Mechanic.create!(name: 'Kara Smith', years_of_experience: 11)
-      mechanic_2 = Mechanic.create!(name: 'Jim Carrey', years_of_experience: 10)
+      mechanic_1 = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+      mechanic_2 = Mechanic.create!(name: 'Jim Carrey', years_experience: 10)
+
+      visit '/mechanics'
 
       expect(page).to have_content(mechanic_1.name)
-      expect(page).to have_content(mechanic_1.years_of_experience)
+      expect(page).to have_content(mechanic_1.years_experience)
       expect(page).to have_content(mechanic_2.name)
-      expect(page).to have_content(mechanic_2.years_of_experience)
+      expect(page).to have_content(mechanic_2.years_experience)
+    end
+
+    it 'shows the average years of experience' do
+      mechanic_1 = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+      mechanic_2 = Mechanic.create!(name: 'Jim Carrey', years_experience: 10)
+
+      visit '/mechanics'
+      
+      expect(page).to have_content(Mechanic.average_years_experience)
     end
   end
 end
