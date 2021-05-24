a4561c472b6ccb861c7fccf3ba549e91a52fa821
Caleb Wittman <calebjwittman1991@gmail.com>
2021-05-24 09:52:22 -0600

    Create more robust test for test two in mechanic index spec

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Mechanics Index Page' do
   before :each do
+    @mech_1 = Mechanic.create!(name: 'Roald', years_experience: 25)
+    @mech_1 = Mechanic.create!(name: 'Larry', years_experience: 10)
+    @mech_1 = Mechanic.create!(name: 'Samantha', years_experience: 30)
+
     visit '/mechanics'
   end
 
@@ -10,11 +14,19 @@ RSpec.describe 'Mechanics Index Page' do
     expect(page).to have_content('All Mechanics')
   end
 
-  xit 'shows all mechanics names and years of experience' do
+  it 'shows all mechanics names and years of experience' do
 
-    expect(page).to have_content()
-    expect(page).to have_content()
-    expect(page).to have_content()
+    within "#mechanic-#{@mech_1}"
+      expect(page).to have_content('Roald')
+      expect(page).to have_content(25)
+
+    within "#mechanic-#{@mech_2}"
+      expect(page).to have_content('Roald')
+      expect(page).to have_content(25)
+
+    within "#mechanic-#{@mech_3}"
+      expect(page).to have_content('Roald')
+      expect(page).to have_content(25)
   end
 
   xit 'shows an average years of experience' do
