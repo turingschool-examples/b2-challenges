0bd1c751a8607bae1fdbcfed75e3c74a656f3b66
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-24 10:07:20 -0600

    Configure models

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,2 +1,7 @@
 class Mechanic < ApplicationRecord
+  has_and_belongs_to_many :rides
+
+  def self.average_experience
+    Mechanic.all.average(:years_experience)
+  end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,3 @@
 class Ride < ApplicationRecord
+  has_and_belongs_to_many :mechanics
 end
