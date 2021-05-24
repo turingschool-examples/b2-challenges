9a555533a3785ceaed079dd91102d7a224a25354
Aliya Merali <merali4@gmail.com>
2021-05-24 09:50:58 -0600

    Add mechanic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Mechanic.destroy_all
+Ride.destroy_all
+
+@mechanic1 = Mechanic.create!(name: "Bob Jones", years_experience: 12)
+@mechanic2 = Mechanic.create!(name: "Sally Fields", years_experience: 23)
+@mechanic3 = Mechanic.create!(name: "Sal Junior", years_experience: 4)
