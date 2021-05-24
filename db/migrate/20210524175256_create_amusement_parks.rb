15657a83322f9ab7bbe53d9a98b1d1169cef0534
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-24 12:04:41 -0600

    Create Amusment Parks and build tests for model

---
+++ db/migrate/20210524175256_create_amusement_parks.rb
@@ -0,0 +1,9 @@
+class CreateAmusementParks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :amusement_parks do |t|
+      t.string :name
+      t.float :price
+      t.timestamps
+    end
+  end
+end
