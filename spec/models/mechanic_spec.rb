640598528d5853b25a3e0c507cacb0ee27819a5e
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-05-24 11:41:38 -0600

    added final model testing for all methods

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Mechanic, type: :model do
     it {should have_many :ride_mechanics}
     it {should have_many(:rides).through(:ride_mechanics)}
   end
+  describe "class methods" do
+    describe "::average_exp" do
+      it 'displays the average experience' do
+        jim = Mechanic.create(name:'Jim Bob', years_experience: 2)
+        sue = Mechanic.create(name:'Sue', years_experience: 3)
+        tim = Mechanic.create(name:'Tim Bob', years_experience: 5)
+        tony = Mechanic.create(name:'Tony Saprano', years_experience: 6)
+      
+        expect(Mechanic.average_exp).to eq(4)
+      end
+    end
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -6,4 +6,43 @@ RSpec.describe Ride, type: :model do
     it {should have_many(:mechanics).through(:ride_mechanics)}
     it {should belong_to :park}
   end
+  describe "class methods" do
+    describe "::open" do
+      it 'only displays the rides that are open' do
+        park = Park.create(name:'Cedar Point', price: 50.00)
+        ride1 = Ride.create(name:'death trap', thrill_rating: 7, open: false, park_id: park.id)
+        ride2 = Ride.create(name:'slow, boring ride', thrill_rating: 1, open: true, park_id: park.id)
+        ride3 = Ride.create(name:'flying death trap', thrill_rating: 10, open: true, park_id: park.id)
+      
+        expect(Ride.open).to eq([ride2, ride3])
+      end
+    end
+    describe "::sort_by_thrill" do
+      it 'displays rides in order of thrill rating' do
+        park = Park.create(name:'Cedar Point', price: 50.00)
+        ride1 = Ride.create(name:'death trap', thrill_rating: 7, open: false, park_id: park.id)
+        ride2 = Ride.create(name:'slow, boring ride', thrill_rating: 1, open: true, park_id: park.id)
+        ride3 = Ride.create(name:'flying death trap', thrill_rating: 10, open: true, park_id: park.id)
+        expect(Ride.sort_by_thrill).to eq([ride3,ride1,ride2])
+      end
+    end
+    describe "::sort_alpha" do
+      it 'displays rides in order alphabetically' do
+        park = Park.create(name:'Cedar Point', price: 50.00)
+        ride1 = Ride.create(name:'death trap', thrill_rating: 7, open: false, park_id: park.id)
+        ride2 = Ride.create(name:'slow, boring ride', thrill_rating: 1, open: true, park_id: park.id)
+        ride3 = Ride.create(name:'flying death trap', thrill_rating: 10, open: true, park_id: park.id)
+        expect(Ride.sort_alpha).to eq([ride1,ride3,ride2])
+      end
+    end
+    describe "::average_thrill" do
+      it 'displays the average thrill rating' do
+        park = Park.create(name:'Cedar Point', price: 50.00)
+        ride1 = Ride.create(name:'death trap', thrill_rating: 7, open: false, park_id: park.id)
+        ride2 = Ride.create(name:'slow, boring ride', thrill_rating: 1, open: true, park_id: park.id)
+        ride3 = Ride.create(name:'flying death trap', thrill_rating: 10, open: true, park_id: park.id)
+        expect(Ride.average_thrill).to eq(6.0)
+      end
+    end
+  end
 end
