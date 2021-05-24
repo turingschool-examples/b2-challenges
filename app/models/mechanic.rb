5e903f70de1b0bc739c194c877ff852f01cbb571
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-05-24 11:54:50 -0400

    Add model testing and model functionality for user story 1

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,3 +1,5 @@
 class Mechanic < ApplicationRecord
-
+  def self.average_years_experience
+    self.average(:years_of_experience)
+  end
 end
---
+++ spec/models/mechanic_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Mechanic do
+  before(:each) do
+    @mechanic_1 = Mechanic.create!(name: 'Kara Smith', years_of_experience: 11)
+    @mechanic_2 = Mechanic.create!(name: 'Lois Iddrisu', years_of_experience: 6)
+    @mechanic_3 = Mechanic.create!(name: 'Bob Stewart', years_of_experience: 7)
+  end
+
+  it 'can find the average years of experience for all mechanics' do
+    expect(Mechanic.average_years_experience).to eq(8)
+  end
+end
