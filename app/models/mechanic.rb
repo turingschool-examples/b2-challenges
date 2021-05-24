4a329a1de1fdecd670288308cc5926b2866e0e10
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-24 10:31:49 -0600

    Implement instance method

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -4,4 +4,8 @@ class Mechanic < ApplicationRecord
   def self.average_experience
     Mechanic.all.average(:years_experience)
   end
+
+  def rides_by_thrill_rating_desc
+    rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
