366b9e33ff562be55bdc57e018e5d05ce327a89e
Suzie K <suziekim.dev@gmail.com>
2021-05-24 11:28:18 -0400

    Red test lists all mechanics and their attributes

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,19 @@
+#spec/features/mechanics/index_spec
+
+require 'rails_helper'
+
+RSpec.describe 'Mechanics index page' do
+  describe 'display' do
+    it 'lists all mechanics and their attributes' do
+      mechanic_1 = Mechanic.create!(name: "Ryan", exp_in_years: 11)
+      mechanic_1 = Mechanic.create!(name: "Bryan", exp_in_years: 5)
+      
+      visit '/mechanics'
+
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Years of Experience")
+      expect(mechanic_1).to appear_before(mechanic_2)
+    end
+  end
+
+end
