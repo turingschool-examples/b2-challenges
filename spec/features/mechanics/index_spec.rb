32e9ac729489eacc6c2590aa7436902db112ca19
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-24 10:01:58 -0600

    Create test for average years of experience

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Mechanics Index Page' do
+  before :each do
+    @mechanic_1 = Mechanic.create!(name: 'Jamie', years_experience: 17)
+    @mechanic_2 = Mechanic.create!(name: 'Thomas', years_experience: 10)
+    @mechanic_3 = Mechanic.create!(name: 'Kelly', years_experience: 26)
+  end
+
   it 'has a header' do
     visit '/mechanics'
 
@@ -8,14 +14,17 @@ RSpec.describe 'Mechanics Index Page' do
   end
 
   it 'has list of mechanics and their years of experience' do
-    @mechanic_1 = Mechanic.create!(name: 'Jamie', years_experience: 17)
-    @mechanic_2 = Mechanic.create!(name: 'Thomas', years_experience: 10)
-    # @mechanic_3 = Mechanic.create!(name: 'Kelly', years_experience: 26)
     visit '/mechanics'
 
     expect(page).to have_content(@mechanic_1.name)
     expect(page).to have_content(@mechanic_1.years_experience)
     expect(page).to have_content(@mechanic_2.name)
     expect(page).to have_content(@mechanic_2.years_experience)
+  end
+
+  it 'has mechanics average years of experience' do
+    visit '/mechanics'
+
+    expect(page).to have_content("Average years of experience: 17.7")
   end
 end
