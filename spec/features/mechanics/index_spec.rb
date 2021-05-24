43d708b89ed381546d759e24c86bb62b1d58dba7
ahenzler <aliciahenzler@outlook.com>
2021-05-24 09:57:40 -0600

    add spec, route, index for mechanic

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,15 @@
+
+
+RSpec.describe 'mechanic index' do
+  before(:each) do
+    mechanic_1 = Mechanic.create!(name: Sam, years_experience: 3)
+    mechanic_2 = Mechanic.create!(name: James, years_experience: 9)
+
+    visit '/mechanics'
+  end
+
+  it 'lists mechanics' do
+    expect(page).to have_content(mechanic_1.name)
+    expect(page).to have_content(mechanic_2.name)
+  end
+end
