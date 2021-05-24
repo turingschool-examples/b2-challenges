68fbe372c78cff9ab5530f52cbaf4830aaf2108d
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-24 18:54:05 +0200

    added rides datbase and created associaitons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_153123) do
+ActiveRecord::Schema.define(version: 2021_05_24_165000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,15 @@ ActiveRecord::Schema.define(version: 2021_05_24_153123) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "rides", force: :cascade do |t|
+    t.string "name"
+    t.integer "thrill_rating"
+    t.boolean "open"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "mechanic_id"
+    t.index ["mechanic_id"], name: "index_rides_on_mechanic_id"
+  end
+
+  add_foreign_key "rides", "mechanics"
 end
