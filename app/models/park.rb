f15a524bec5e349732771224d7ed5218a76e5d3f
Aliya Merali <merali4@gmail.com>
2021-05-24 11:51:22 -0600

    Move model methods from park model to ride model spec

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,4 +1,7 @@
 class Park < ApplicationRecord
   has_many :rides, dependent: :destroy
 
+  def self.rides_by_name
+    order(:name)
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -6,17 +6,28 @@ RSpec.describe Ride, type: :model do
     it {should belong_to :park}
   end
 
+  before :each do
+    @mechanic1 = Mechanic.create!(name: "Bob Jones", years_experience: 12)
+    @park = Park.create!(name: "Six Flags Great Adventure", admission_price: 49.99)
+
+    @ride1 = @mechanic1.rides.create!(name: "Teacups", thrill_rating: 3, open: true, park_id: @park.id)
+    @ride2 = @mechanic1.rides.create!(name: "Twirl-a-whirl", thrill_rating: 7, open: true, park_id: @park.id)
+    @ride3 = @mechanic1.rides.create!(name: "Skull Mountain", thrill_rating: 9, open: false, park_id: @park.id)
+  end
+
   describe 'class methods' do
     it '#open_rides_by_thrill returns a list of open rides sorted by thrill rating' do
-      @mechanic1 = Mechanic.create!(name: "Bob Jones", years_experience: 12)
-      @park = Park.create!(name: "Six Flags Great Adventure", admission_price: 49.99)
-
-      @ride1 = @mechanic1.rides.create!(name: "Teacups", thrill_rating: 3, open: true, park_id: @park.id)
-      @ride2 = @mechanic1.rides.create!(name: "Twirl-a-whirl", thrill_rating: 7, open: true, park_id: @park.id)
-      @ride3 = @mechanic1.rides.create!(name: "Skull Mountain", thrill_rating: 9, open: false, park_id: @park.id)
 
       expect(Ride.open_rides_by_thrill).to eq([@ride2, @ride1])
       expect(@mechanic1.rides.open_rides_by_thrill).to eq([@ride2, @ride1])
+    end
+
+    it '#rides_by_name returns rides in alphabetical order' do
+      expect(Ride.rides_by_name).to eq([@ride3, @ride1, @ride2])
+    end
+
+    it '#avg_thrill returns average thrill rating for a collection of rides, rounded to 2' do
+      expect(Ride.avg_thrill).to eq(6.33)
     end
   end
 end
