d108bd06941c3f2110416e89ed19909d779e4ebc
Jamison Griffith <jamison.griffith@gmail.com>
2021-05-24 10:31:31 -0600

    Adds avg_exp model method

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Mechanic do
     it {should have_many(:rides).through(:ride_mechanics)}
   end
 
+  describe 'functions' do
+    it 'returns average years of experience' do
+      @mech = Mechanic.create!(name: "Marty", years_experience: 3)
+      @mech2 = Mechanic.create!(name: "Gregor", years_experience: 15)
+      @mech3 = Mechanic.create!(name: "Sarah", years_experience: 7)
+      expected_average = (3 + 15 + 7) / 3.to_f
+      expect(Mechanic.all.avg_exp).to eq expected_average.round
+    end
+  end
 end
