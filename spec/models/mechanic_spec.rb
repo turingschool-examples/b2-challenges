f366f3eec849233c9f46f7341b4f4b8faa69b6e4
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-05-24 10:04:42 -0600

    Add models and model tests

---
+++ spec/models/mechanic_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Mechanic, type: :model do
+  before (:each) do
+    @ride_1 = Ride.create(name: 'Scrambler', thrill_rating: 8, open: true)
+    @ride_2 = Ride.create(name: 'Tea Cups', thrill_rating: 2, open: true)
+    @ride_3 = Ride.create(name: 'Log Ride', thrill_rating: 4, open: false)
+    @ride_4 = Ride.create(name: 'Tower of Doom', thrill_rating: 10, open: true)
+
+    @mechanic_1 = Mechanic.create!(name: "Suzie Q", years_experience: 2)
+    @mechanic_2 = Mechanic.create!(name: "Jane Doe", years_experience: 4)
+    @mechanic_3 = Mechanic.create!(name: "Barak Obama", years_experience: 6)
+
+    @mechanic_ride_1 = MechanicRide.create!(ride: @ride_1, mechanic: @mechanic_1)
+    @mechanic_ride_2 = MechanicRide.create!(ride: @ride_2, mechanic: @mechanic_2)
+    @mechanic_ride_3 = MechanicRide.create!(ride: @ride_3, mechanic: @mechanic_3)
+    @mechanic_ride_4 = MechanicRide.create!(ride: @ride_4, mechanic: @mechanic_3)
+  end
+
+
+  describe 'relationships' do
+    it {should have_many :mechanic_rides}
+    it {should have_many(:rides).through(:mechanic_rides)}
+  end
+
+  describe 'instance methods' do
+  end
+
+  describe 'class methods' do
+  end
+end
