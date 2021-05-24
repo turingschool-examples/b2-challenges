eaf40321294cd8a62276c7cbe79cce3f29f7d207
Aliya Merali <merali4@gmail.com>
2021-05-24 09:52:27 -0600

    Add test to mechanic model spec to pull avg_yrs_exp method into model

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Mechanic, type: :model do
   describe 'relationships' do
     it {should have_many :rides}
   end
+
+  describe 'class methods' do
+    it '#avg_yrs_exp returns the average of years_experience' do
+      @mechanic1 = Mechanic.create!(name: "Bob Jones", years_experience: 12)
+      @mechanic2 = Mechanic.create!(name: "Sally Fields", years_experience: 23)
+      @mechanic3 = Mechanic.create!(name: "Sal Junior", years_experience: 4)
+
+      expect(Mechanic.avg_yrs_exp).to eq(13.0)
+    end
+  end
 end
