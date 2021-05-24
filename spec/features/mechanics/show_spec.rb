2687df9cebf776dfa9fc197a8d99d8b26c93a710
NoahZinter <noah.zinter@gmail.com>
2021-05-24 11:51:25 -0600

    Test order on show page

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -3,10 +3,26 @@ require 'rails_helper'
 describe 'mechanics show page' do
   before :each do
     @tim = Mechanic.create!(name:'Tim', experience: 12)
+    @scrambler = Ride.create!(name:'Scrambler', open: false, thrill_rating: 5)
+    @rambler = Ride.create!(name:'Rambler', open: true, thrill_rating: 6)
+    @ambler = Ride.create!(name:'Ambler', open: true, thrill_rating: 7)
+
+    MechanicRide.create!(mechanic: @tim, ride: @scrambler)
+    MechanicRide.create!(mechanic: @tim, ride: @rambler)
+    MechanicRide.create!(mechanic: @tim, ride: @ambler)
     visit "/mechanics/#{@tim.id}"
+
   end
   it 'displays a mechanics name and age' do
     expect(page).to have_content('Mechanic Name: Tim')
     expect(page).to have_content('Mechanic Experience: 12 years')
+  end
+  it 'displays the names only of open rides being worked' do
+    expect(page).not_to have_content('Tim is working on: Scrambler')
+    expect(page).to have_content('Tim is working on: Rambler')
+    expect(page).to have_content('Which has a thrill rating of 6')
+  end
+  it 'displays the rides in descending order of thrill rating' do
+    expect('Tim is working on: Ambler').to appear_before('Tim is working on: Rambler')
   end
 end
