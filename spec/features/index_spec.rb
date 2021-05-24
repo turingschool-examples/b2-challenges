ea7603d91cba094c29c1bc16aa9de016a056da95
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-24 09:53:16 -0600

    Feature test for index page of mechanics

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -2,11 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'The index page for mechanics,' do
 
-  it 'should have a header'
+  before :all do
+    @mech_1 = Mechanic.create(name: 'Bob', years_experience: 12)
+    @mech_2 = Mechanic.create(name: 'Rich', years_experience: 6)
+  end
 
-  it 'shows average years of experience across all mechanics'
+  before :each do
+    visit '/mechanics'
+  end
+
+  after :all do
+    Mechanic.destroy_all
+  end
+
+  it 'should have a header' do
+    expect(page).to have_selector('h2', with: 'All Mechanics')
+  end
+
+  it 'shows average years of experience across all mechanics' do
+    within '#mechanics_section' do
+      expect(page).to have_selector('h3', with: Mechanic.average_experience)
+    end
+  end
 
   describe 'list of mechanics,' do
-    it 'shows all the mechanics with their name and experience'
+    it 'shows all the mechanics with their name and experience' do
+      expect(page).to have_selector('#mechanics_list')
+
+      within "#mechanic_#{@mech_1.id}" do
+        expect(page).to have_content(@mech_1.name)
+        expect(page).to have_content(@mech_1.years_experience)
+      end
+
+      within "#mechanic_#{@mech_2.id}" do
+        expect(page).to have_content(@mech_2.name)
+        expect(page).to have_content(@mech_2.years_experience)
+      end
+    end
   end
 end
