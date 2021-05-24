48d248a4bdfb0b45493b0efdf35c12922fc6b095
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-24 11:20:36 -0600

    Implement feature to add a ride to workload

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,12 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def update
+    @mechanic = Mechanic.find(params[:mechanic_id])
+    if params[:add_ride]
+      @mechanic.rides << Ride.find(params[:add_ride][:ride_id])
+    end
+    redirect_to "/mechanics/#{@mechanic.id}"
+  end
 end
