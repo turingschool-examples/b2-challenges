613ecd2b3024a6e47b7441b73aaef74097d25ca1
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-05-24 11:59:16 -0600

    ride has dependency on park, added park_id to rides for mechanics tests, now passes

--- spec/features/mechanic/show_spec.rb
+++ spec/features/mechanic/show_spec.rb
@@ -11,9 +11,10 @@ require 'rails_helper'
 describe 'Mechanic show page' do
   it "has the mech's name and experience and the names of the rides they are working on, only open rides, listed by thrill rating" do
     jim = Mechanic.create(name:'Jim Bob', years_experience: 2)
-    ride1 = Ride.create(name:'death trap', thrill_rating: 7, open: false)
-    ride2 = Ride.create(name:'slow, boring ride', thrill_rating: 1, open: true)
-    ride3 = Ride.create(name:'flying death trap', thrill_rating: 10, open: true)
+    park = Park.create(name:'Cedar Point', price: 50.00)
+    ride1 = Ride.create(name:'death trap', thrill_rating: 7, open: false, park_id: park.id)
+    ride2 = Ride.create(name:'slow, boring ride', thrill_rating: 1, open: true, park_id: park.id)
+    ride3 = Ride.create(name:'flying death trap', thrill_rating: 10, open: true, park_id: park.id)
     thing = RideMechanic.create(mechanic: jim, ride: ride1)
     otherthing = RideMechanic.create(mechanic: jim, ride: ride2)
     otherotherthing = RideMechanic.create(mechanic: jim, ride: ride3)
@@ -21,6 +22,7 @@ describe 'Mechanic show page' do
     visit "/mechanics/#{jim.id}"
     expect(page).to have_content('Jim Bob')
     expect(page).to have_content(2)
+    binding.pry
     expect(page).to have_content('slow, boring ride')
     expect(page).to have_content('flying death trap')
     expect(page).to have_content(1)
@@ -38,9 +40,10 @@ describe 'Mechanic show page' do
 
   it 'has a link to a form to add a ride to mechanic. give ID of existing ride, back to mech show page, shows updated with new ride' do
     jim = Mechanic.create(name:'Jim Bob', years_experience: 2) 
-    ride1 = Ride.create(name:'death trap', thrill_rating: 7, open: false)
-    ride2 = Ride.create(name:'slow, boring ride', thrill_rating: 1, open: true)
-    ride3 = Ride.create(name:'flying death trap', thrill_rating: 10, open: true)
+    park = Park.create(name:'Cedar Point', price: 50.00)
+    ride1 = Ride.create(name:'death trap', thrill_rating: 7, open: false, park_id: park.id)
+    ride2 = Ride.create(name:'slow, boring ride', thrill_rating: 1, open: true, park_id: park.id)
+    ride3 = Ride.create(name:'flying death trap', thrill_rating: 10, open: true, park_id: park.id)
     thing = RideMechanic.create(mechanic: jim, ride: ride1)
     otherthing = RideMechanic.create(mechanic: jim, ride: ride2)
     visit "/mechanics/#{jim.id}"
