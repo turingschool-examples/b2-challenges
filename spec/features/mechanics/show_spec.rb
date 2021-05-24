56b369e75f4e0cf8c420fe5ef154d0abb5a04087
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-24 19:16:03 +0200

    create test for mechanic show page

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -5,14 +5,22 @@ RSpec.describe 'mechanics show page', type: :feature do
     @mechanic1 = Mechanic.create!( name:"Joe Meek",
                                    years_experience: 10
                                   )
-    @mechanic2 = Mechanic.create!( name:"Sara Fix",
-                                     years_experience: 6
-                                  )
+    @ride1     = @mechanic1.rides.create!( name:"Thunder Dome",
+                                           thrill_rating: 5,
+                                           open: false
+                                          )
+    @ride2     = @mechanic1.rides.create!( name:"Wet Willy",
+                                           thrill_rating: 3,
+                                           open: true
+                                          )
   end
-  it "displays a mechanics information " do
+  it "displays a mechanics information and the rides worked on " do
     visit "/mechanics/#{@mechanic1.id}"
-
+    save_and_open_page
     expect(page).to have_content(@mechanic1.name)
+    expect(page).to have_content(@ride1.name)
     expect(page).to have_content("Years Experience: 10")
+    expect(page).to have_content("Years Experience: 10")
+
   end
 end
