4682cb5a33fb3e103a25d30e498a88c0d6591a7a
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-24 11:43:29 -0600

    Remove comments and complete US 3

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,23 +1,3 @@
-# As a user,
-# When I go to a mechanics show page
-# I see a form to add a ride to their workload
-# When I fill in that field with an id of an existing ride and hit submit
-# I’m taken back to that mechanic's show page
-# And I see the name of that newly added ride on this mechanics show page
-#
-# Ex:
-# Mechanic: Kara Smith
-# Years of Experience: 11
-#
-# Current rides they’re working on:
-# The Frog Hopper
-# Fahrenheit
-# The Kiss Raise
-#
-# Add a ride to workload:
-# Ride Id: _pretend_this_is_a_textfield_
-# Submit
-
 require 'rails_helper'
 
 RSpec.describe 'mechanic show page' do
