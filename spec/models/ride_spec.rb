2eb0d6dbe6cc6711483fc182bedd60c497a4a75d
Suzie K <suziekim.dev@gmail.com>
2021-05-24 12:25:07 -0400

    Red test ::show_only_open method

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Ride, type: :model do
   describe 'relationships' do
     it {should belong_to :mechanic}
   end
+
+  before :each do
+    @mechanic_1 = Mechanic.create!(name: "Ryan", exp_in_years: 11)
+    @hurler = @mechanic_1.rides.create!(name: "The Hurler", thrill_rating: 7, open: true)
+    @viking = @mechanic_1.rides.create!(name: "The Viking", thrill_rating: 5, open: true)
+    @teacups = @mechanic_1.rides.create!(name: "Spinning Teacup", thrill_rating: 3, open: true)
+    @bouncy_house = @mechanic_1.rides.create!(name: "The Bouncy House", thrill_rating: 1, open: false)
+  end
+
+  describe 'class methods' do
+    it '::show_only_open' do
+
+      expect(Ride.show_only_open).to eq([@hurler, @viking, @teacups])
+    end
+  end
 end
