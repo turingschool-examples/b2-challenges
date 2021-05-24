6962b1ce9192d1bc6d9944a9399d2bf0b61a77a3
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-24 10:10:55 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Mechanic.create(name: Faker::Name.name, years_experience: rand(1..15))
+end
