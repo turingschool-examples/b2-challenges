ddb0af032c119fa46954526595b032b113e79d6c
ahenzler <aliciahenzler@outlook.com>
2021-05-24 10:18:40 -0600

    add mechanic index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-# Mechanic.destroy_all
+Mechanic.destroy_all
 
 
-# @mechanic_1 = Mechanic.create!(:)
+@mechanic_1 = Mechanic.create!(name: "Sam", years_experience: 3)
+@mechanic_2 = Mechanic.create!(name: "James", years_experience: 9)
--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,14 +2,14 @@
 
 RSpec.describe 'mechanic index' do
   before(:each) do
-    mechanic_1 = Mechanic.create!(name: Sam, years_experience: 3)
-    mechanic_2 = Mechanic.create!(name: James, years_experience: 9)
+    @mechanic_1 = Mechanic.create!(name: "Sam", years_experience: 3)
+    @mechanic_2 = Mechanic.create!(name: "James", years_experience: 9)
 
     visit '/mechanics'
   end
 
   it 'lists mechanics' do
-    expect(page).to have_content(mechanic_1.name)
-    expect(page).to have_content(mechanic_2.name)
+    expect(page).to have_content(@mechanic_1.name)
+    expect(page).to have_content(@mechanic_2.name)
   end
 end
