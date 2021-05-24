6356bf0b63ce9e544ed2636e19405828989d5746
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-24 09:39:58 -0600

    Write inital expected tests

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'The index page for mechanics,' do
+
+  it 'should have a header'
+
+  it 'shows average years of experience across all mechanics'
+
+  describe 'list of mechanics,' do
+    it 'shows all the mechanics with their name and experience'
+  end
+end
