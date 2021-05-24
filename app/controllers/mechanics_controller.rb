a5bc7bc842317769df25ccd3171147cc8de65746
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-24 10:48:36 -0600

    Add show method

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,10 @@ class MechanicsController < ApplicationController
   def index
     @mechanics = Mechanic.all
   end
+
+  def show
+    @mechanic = Mechanic.find(params[:id])
+    # @open_rides = @mechanic.rides.where(open: true)
+    # @open_rides.thrill_rating_order
+  end
 end
