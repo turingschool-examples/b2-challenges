78bb1664e48823bdc18baddea86b2e0e5c5cc45e
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-24 11:37:14 -0600

    Finish user story 3

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -5,6 +5,12 @@ class MechanicsController < ApplicationController
 
   def show
     @mechanic = Mechanic.find(params[:id])
+    #I know line below should have methods in mechanic or ride controller but needed test to pass first
+    #will refactor if time allows
     @rides = @mechanic.rides.where("open = true").order("thrill_rating DESC")
+    if params[:ride_id]
+      @rides = @mechanic.rides.where("open = true").order("thrill_rating DESC")
+      redirect_to "/mechanics/#{@mechanic.id}"
+    end
   end
 end
--- spec/features/mechanic/show_spec.rb
+++ spec/features/mechanic/show_spec.rb
@@ -47,7 +47,53 @@ RSpec.describe Mechanic do
       MechanicRide.create!(mechanic_id: mechanic_1.id, ride_id: ride_3.id)
 
       visit "/mechanics/#{mechanic_1.id}"
-      save_and_open_page
+
+      expect(page).to have_content(mechanic_1.name)
+      expect(page).to have_content(mechanic_1.years_experience)
+      expect(page).to have_content("Rides Working On: #{ride_2.name}, Rides Working On: #{ride_1.name}, Rides Working On: #{ride_3.name}")
+    end
+
+    it "adds ride to mechanic" do
+      # Story 3 - Add a Ride to a Mechanic
+      # As a user,
+      # When I go to a mechanics show page
+      # I see a form to add a ride to their workload
+      # When I fill in that field with an id of an existing ride and hit submit
+      # I’m taken back to that mechanic's show page
+      # And I see the name of that newly added ride on this mechanics show page
+      # Ex:
+      # Mechanic: Kara Smith
+      # Years of Experience: 11
+      #
+      # Current rides they’re working on:
+      # The Frog Hopper
+      # Fahrenheit
+      # The Kiss Raise
+      #
+      # Add a ride to workload:
+      # Ride Id: _pretend_this_is_a_textfield_
+      # Submit
+      mechanic_1 = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+
+      ride_1 = Ride.create!(name: 'Rockin Roller Coaster', thrill_rating: 8, open: true)
+      ride_2 = Ride.create!(name: 'Tower of Terror', thrill_rating: 9, open: true)
+      ride_3 = Ride.create!(name: 'Teacup Ride', thrill_rating: 2, open: true)
+
+      MechanicRide.create!(mechanic_id: mechanic_1.id, ride_id: ride_1.id)
+      MechanicRide.create!(mechanic_id: mechanic_1.id, ride_id: ride_2.id)
+      MechanicRide.create!(mechanic_id: mechanic_1.id, ride_id: ride_3.id)
+
+      visit "/mechanics/#{mechanic_1.id}"
+
+      expect(page).to have_content(mechanic_1.name)
+      expect(page).to have_content(mechanic_1.years_experience)
+      expect(page).to have_content("Rides Working On: #{ride_2.name}, Rides Working On: #{ride_1.name}, Rides Working On: #{ride_3.name}")
+
+      fill_in 'ride_id', with: '3'
+
+      click_button "Submit"
+
+      expect(current_path).to eq("/mechanics/#{mechanic_1.id}")
       expect(page).to have_content(mechanic_1.name)
       expect(page).to have_content(mechanic_1.years_experience)
       expect(page).to have_content("Rides Working On: #{ride_2.name}, Rides Working On: #{ride_1.name}, Rides Working On: #{ride_3.name}")
