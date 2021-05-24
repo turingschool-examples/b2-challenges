678258f9bf177404a1418feef9087eec8294374b
Aliya Merali <merali4@gmail.com>
2021-05-24 09:53:19 -0600

    Pull avg_yrs_exp method into model

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,7 +2,7 @@ class MechanicsController < ApplicationController
 
   def index
     @mechanics = Mechanic.all
-    @avg_yrs_exp = Mechanic.average(:years_experience)
+    @avg_yrs_exp = Mechanic.avg_yrs_exp
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,3 +1,7 @@
 class Mechanic < ApplicationRecord
   has_many :rides, dependent: :destroy
+
+  def self.avg_yrs_exp
+    average(:years_experience)
+  end
 end
