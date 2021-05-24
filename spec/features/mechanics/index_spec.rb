0510499491ae26e4ab4b408c759f8383ad336f65
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-24 17:41:44 +0200

    create index spec and index page test

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'mechanics index page', type: :feature do
+    before :each do
+    @mechanic1 = Mechanic.create!( name:"Joe Meek",
+                                   years_experience: 10,
+                                  )
+    @mechanic2 = FlowerShop.create!( name:"Sara Fix",
+                                     years_experience: 6,
+                                  )
+  end
+  it "can see the mechanics header and a list of all the mechanics and information" do
+    visit "/mechanics"
+
+    expect(page).to have_content('All Mechanics')
+    expect(page).to have_content(@mechanic1.name)
+    expect(page).to have_content(@mechanic2.name)
+  end
+end
