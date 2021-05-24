20fc44e25e285af2fd1a042e0cb36c4e32f41b18
Caleb Wittman <calebjwittman1991@gmail.com>
2021-05-24 12:18:39 -0600

    Create functionality for first part of extension user story

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
   get '/mechanics/:id', to: 'mechanics#show'
-  post 'mechanics/:id', to: 'mechanics#show'
+  post '/mechanics/:id', to: 'mechanics#show'
+
+  get '/amusement_parks/:id', to: 'amusement_parks#show'
 end
--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Amusement Park Show Page' do
   before :each do
     @park_1 = AmusementPark.create!(name: 'Super Fun Park', admission_price: 12)
-    @park_1 = AmusementPark.create!(name: 'Not So Super Fun Park', admission_price: 10)
+    @park_2 = AmusementPark.create!(name: 'Not So Super Fun Park', admission_price: 10)
 
     visit "/amusement_parks/#{@park_1.id}"
   end
