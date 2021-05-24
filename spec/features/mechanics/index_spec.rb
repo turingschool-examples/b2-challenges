954d7fd2abbf7660b25e09340c8a3c80f1ebc427
Zach T <ztrokey@gmail.com>
2021-05-24 10:17:19 -0600

    add mechanics to view page and tests

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'the mechanic index' do
   it 'has a header saying All Mechanics' do
-    mechanic_1 = Mechanic.create!(name: 'Fix it Felix', years_experience: 25)
 
     visit '/mechanics'
 
     expect(page).to have_content('All Mechanics')
+  end
+  it 'lists all of the mechanics and their attributes' do
+    mechanic_1 = Mechanic.create!(name: 'Fix it Felix Jr.', years_experience: 20)
+    mechanic_2 = Mechanic.create!(name: 'Bob the Builder', years_experience: 10)
+
+    visit '/mechanics'
+
+    expect(page).to have_content(mechanic_1.name)
+    expect(page).to have_content(mechanic_2.name)
+    expect(page).to have_content(mechanic_2.years_experience)
+    expect(page).to have_content(mechanic_2.years_experience)
   end
 end
