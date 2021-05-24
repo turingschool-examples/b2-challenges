08984fd998531534e7d7834b505cf4eb1bc09d78
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-24 20:07:12 +0200

    add new ride form

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,6 +6,10 @@ class MechanicsController < ApplicationController
 
   def show
     @mechanic = Mechanic.find(params[:id])
-    @rides = Ride.all
+    @rides = Ride.where(open: true)
+    #@rides = Ride.order(thrill_rating: :desc)
+    Ride.create(name: params[:name])
   end
+
+
 end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -10,17 +10,32 @@ RSpec.describe 'mechanics show page', type: :feature do
                                            open: false
                                           )
     @ride2     = @mechanic1.rides.create!( name:"Wet Willy",
-                                           thrill_rating: 3,
+                                           thrill_rating: 6,
+                                           open: true
+                                          )
+    @ride3     = @mechanic1.rides.create!( name:"Nitro",
+                                           thrill_rating: 7,
                                            open: true
                                           )
   end
   it "displays a mechanics information and the rides worked on " do
     visit "/mechanics/#{@mechanic1.id}"
-    save_and_open_page
-    expect(page).to have_content(@mechanic1.name)
-    expect(page).to have_content(@ride1.name)
-    expect(page).to have_content("Years Experience: 10")
-    expect(page).to have_content("Years Experience: 10")
 
+    expect(page).to have_content(@mechanic1.name)
+    expect(page).to have_content("Years Experience: 10")
+    expect(page).to have_content(@ride2.name)
+    expect(page).to_not have_content(@ride1.name)
+    #expect(@ride3.name).to appear_before(@ride2.name)
+  end
+
+  it "has form to add ride" do
+    visit "/mechanics/#{@mechanic1.id}"
+
+    fill_in 'name:', with: 'Scream Machine'
+    click_on 'Submit'
+
+    expect(current_path).to eq("/mechanics/#{@mechanic1.id}")
+      expect(page).to have_content('Scream Machine')
+      expect(page).to have_button("Submit")
   end
 end
