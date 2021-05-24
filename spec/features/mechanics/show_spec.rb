9c67ff2c9c84e6a07d697536f7c989641e98dceb
Suzie K <suziekim.dev@gmail.com>
2021-05-24 13:26:31 -0400

    Red test adds ride to application

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe 'Mechanics show page' do
       @viking = @mechanic_1.rides.create!(name: "The Viking", thrill_rating: 2, open: true)
       @teacups = @mechanic_1.rides.create!(name: "Spinning Teacup", thrill_rating: 3, open: true)
       @bouncy_house = @mechanic_1.rides.create!(name: "The Bouncy House", thrill_rating: 1, open: false)
+      @mechanic_2 = Mechanic.create!(name: "Bryan", exp_in_years: 5)
+      @haunted_house = @mechanic_2.rides.create!(name: "Haunted House", thrill_rating: 10, open: true)
     end
 
     it 'lists mechanic and their attributes' do
@@ -35,6 +37,19 @@ RSpec.describe 'Mechanics show page' do
       expect(@hurler.name).to appear_before(@teacups.name)
       expect(@hurler.name).to appear_before(@viking.name)
       expect(@teacups.name).to appear_before(@viking.name)
+    end
+
+    it 'adds ride to application' do
+      visit "/mechanics/#{@mechanic_1.id}"
+
+      expect(page).to_not have_content(@haunted_house.id)
+      expect(page).to have_content("Add a ride to workload:")
+      expect(page).to have_content("Ride ID:")
+      
+      fill_in "Ride ID:", with: "#{@haunted_house.id}"
+      click_on("Add New Ride")
+      save_and_open_page
+      expect(page).to have_content(@haunted_house.name)
     end
   end
 end
