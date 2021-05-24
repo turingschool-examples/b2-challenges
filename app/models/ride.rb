f22c81e30952f9036f5952d655dae7fffa66dada
Zach T <ztrokey@gmail.com>
2021-05-24 11:27:33 -0600

    add open rides to mechanic show

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,7 @@
 class Ride < ApplicationRecord
   belongs_to :mechanic
+
+  def self.open_rides
+    Ride.where.not(open: false)
+  end
 end
---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'the mechanic show' do
+  it 'lists the mechanics attributes' do
+    mechanic_1 = Mechanic.create!(name: 'Fix it Felix Jr.', years_experience: 20)
+    mechanic_2 = Mechanic.create!(name: 'Bob the Builder', years_experience: 10)
+    ride_1 = mechanic_1.rides.create!(name: 'Crazy Coaster', thrill_rating: 10, open: true)
+    ride_2 = mechanic_1.rides.create!(name: 'Kiddy Carts', thrill_rating: 2, open: true)
+    ride_3 = mechanic_1.rides.create!(name: 'Tower of Doom', thrill_rating: 20, open: false)
+
+    visit "/mechanics/#{mechanic_1.id}"
+    
+    expect(page).to have_content(mechanic_1.name)
+    expect(page).to have_content(mechanic_1.years_experience)
+    expect(page).to have_content('Crazy Coaster')
+    expect(page).to have_content('Kiddy Carts')
+    expect(page).to_not have_content('Tower of Doom')
+  end
+end
+
+
