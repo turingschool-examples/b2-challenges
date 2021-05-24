c970668fcbcb9d6ff028444a113c3365c59c5297
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-05-24 10:52:18 -0600

    setup park migration, association to rides, model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_153042) do
+ActiveRecord::Schema.define(version: 2021_05_24_165018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
