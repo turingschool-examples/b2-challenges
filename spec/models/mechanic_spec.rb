4561a119b3386a8f58781f4eb5c11f60ec702435
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-24 10:01:40 -0600

    Set up tests for method avg_yrs_experience

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Mechanic do
     it {should have_many :ride_mechanics}
     it {should have_many(:rides).through(:ride_mechanics)}
   end
+
+  describe 'class methods' do
+    describe '::avg_years_experience' do
+      it 'returns the average years experience of all mechanics' do
+        jim = Mechanic.create!(name: "Jim", years_experience: 17)
+        xena = Mechanic.create!(name: "Xena", years_experience: 23)
+
+        expect(Mechanic.avg_years_experience).to eq 20
+      end
+    end
+  end
 end
