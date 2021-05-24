52e02d682de12790dd834826a872f72ecf7a5cb7
Jamison Griffith <jamison.griffith@gmail.com>
2021-05-24 10:21:59 -0600

    Implements user story 1 through average exp

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -3,18 +3,31 @@ require 'rails_helper'
 RSpec.describe 'mechanics index page' do
 
   before :all do
-    @mech = Mechanic.create!(name: "Marty", years_of_experience: 3)
+    @mech = Mechanic.create!(name: "Marty", years_experience: 3)
+    @mech2 = Mechanic.create!(name: "Gregor", years_experience: 15)
+    @mech3 = Mechanic.create!(name: "Sarah", years_experience: 7)
     @ride1 = Ride.create!(name: "Hurly Whirley", open: false, thrill_rating: 10)
   end
 
   describe 'index etc' do
-    #Story 1 - Mechanic Index Page
-    #As a user,
-    #When I visit the mechanics index page
-    #I see a header saying “All Mechanics”
-    #And I see a list of all mechanic’s names and their years of experience
-    #And I see the average years of experience across all mechanics
-
+    it 'lists mechanics and their details' do
+      #Story 1 - Mechanic Index Page
+      #As a user,
+      #When I visit the mechanics index page
+      #I see a header saying “All Mechanics”
+      #And I see a list of all mechanic’s names and their years of experience
+      #And I see the average years of experience across all mechanics
+      visit '/mechanics'
+      within 'h2' do
+        expect(page).to have_content "All Mechanics"
+      end
+      within 'ul' do
+        expect(page).to have_content @mech.name
+        expect(page).to have_content @mech2.years_experience
+      end
+      expected_average = (3 + 15 + 7) / 3
+      expect(page).to have_content("Our mechanics have an average of #{expected_average} years of experience.")
+    end
   end
 
 end
