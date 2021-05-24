26ffd3dc531df1fb7845db5f9da64b81acd7ea93
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-24 12:15:40 -0600

    Update foreign key to be on rides table for amusement parks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_180623) do
+ActiveRecord::Schema.define(version: 2021_05_24_181458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_180623) do
     t.float "price_of_admission"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "ride_id"
-    t.index ["ride_id"], name: "index_amusement_parks_on_ride_id"
   end
 
   create_table "mechanic_rides", force: :cascade do |t|
@@ -46,9 +44,11 @@ ActiveRecord::Schema.define(version: 2021_05_24_180623) do
     t.boolean "open"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "amusement_park_id"
+    t.index ["amusement_park_id"], name: "index_rides_on_amusement_park_id"
   end
 
-  add_foreign_key "amusement_parks", "rides"
   add_foreign_key "mechanic_rides", "mechanics"
   add_foreign_key "mechanic_rides", "rides"
+  add_foreign_key "rides", "amusement_parks"
 end
