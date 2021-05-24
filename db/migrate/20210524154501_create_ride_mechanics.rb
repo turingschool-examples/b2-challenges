7dd5d3b8276cc2e8df057ac56514fad80b5a28a0
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-05-24 08:47:36 -0700

    Create migrations and joins table

---
+++ db/migrate/20210524154501_create_ride_mechanics.rb
@@ -0,0 +1,10 @@
+class CreateRideMechanics < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ride_mechanics do |t|
+      t.references :ride, foreign_key: true
+      t.references :mechanics, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
