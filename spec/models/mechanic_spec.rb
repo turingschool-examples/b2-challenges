3b51c833e0f272a27415f50d53a456a8399421f5
Richard DeSilvey <rdesilvey@gmail.com>
2021-05-24 09:37:46 -0600

    Test for relationships

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
+  describe 'relationships' do
+    it {should have_and_belong_to_many :rides}
+  end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Ride, type: :model do
+  describe 'relationships' do
+    it {should have_and_belong_to_many :mechanics}
+  end
 end
