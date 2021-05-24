40e59ba6857ac9df2e4f373313670c52e14e922b
Suzie K <suziekim.dev@gmail.com>
2021-05-24 12:48:08 -0400

    Red test ::sort_by_thrill level

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Ride, type: :model do
   before :each do
     @mechanic_1 = Mechanic.create!(name: "Ryan", exp_in_years: 11)
     @hurler = @mechanic_1.rides.create!(name: "The Hurler", thrill_rating: 7, open: true)
-    @viking = @mechanic_1.rides.create!(name: "The Viking", thrill_rating: 5, open: true)
+    @viking = @mechanic_1.rides.create!(name: "The Viking", thrill_rating: 2, open: true)
     @teacups = @mechanic_1.rides.create!(name: "Spinning Teacup", thrill_rating: 3, open: true)
     @bouncy_house = @mechanic_1.rides.create!(name: "The Bouncy House", thrill_rating: 1, open: false)
   end
@@ -18,6 +18,11 @@ RSpec.describe Ride, type: :model do
     it '::show_only_open' do
 
       expect(Ride.show_only_open).to eq([@hurler, @viking, @teacups])
+    end
+
+    it '::sort_by_thrill_level' do
+
+      expect(Ride.sort_by_thrill_level).to eq([@hurler, @teacups, @viking, @bouncy_house])
     end
   end
 end
