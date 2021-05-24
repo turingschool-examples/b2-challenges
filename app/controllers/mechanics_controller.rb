2c36086e29d10fa46f5f6b60f6784fbf66f1978f
Aliya Merali <merali4@gmail.com>
2021-05-24 10:13:10 -0600

    Add ordlerly gem, route, method, and view for mechanics show page

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -5,4 +5,9 @@ class MechanicsController < ApplicationController
     @avg_yrs_exp = Mechanic.avg_yrs_exp
   end
 
+  def show
+    @mechanic = Mechanic.find(params[:id])
+    @rides = @mechanic.rides.where('open = true').order(thrill_rating: :desc)
+  end
+
 end
