534d4c2ec72385bbc6490f1c3b21f49fab645366
Suzie K <suziekim.dev@gmail.com>
2021-05-24 11:52:36 -0400

    Red test list average of years of exp

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -4,16 +4,24 @@ require 'rails_helper'
 
 RSpec.describe 'Mechanics index page' do
   describe 'display' do
+    before :each do
+      @mechanic_1 = Mechanic.create!(name: "Ryan", exp_in_years: 11)
+      @mechanic_2 = Mechanic.create!(name: "Bryan", exp_in_years: 5)
+    end
     it 'lists all mechanics and their attributes' do
-      mechanic_1 = Mechanic.create!(name: "Ryan", exp_in_years: 11)
-      mechanic_2 = Mechanic.create!(name: "Bryan", exp_in_years: 5)
+
       
       visit '/mechanics'
    
       expect(page).to have_content("Name")
       expect(page).to have_content("Years of Experience")
-      expect(mechanic_1.name).to appear_before(mechanic_2.name)
+      expect(@mechanic_1.name).to appear_before(@mechanic_2.name)
+    end
+
+    it 'lists average of experience across all mechanics' do
+      visit '/mechanics'
+
+      expect(page).to have_content("Average Years of Experience: 8")
     end
   end
-
 end
