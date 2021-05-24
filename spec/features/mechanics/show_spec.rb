7ad1968b295a615f93c42381b9cd211af9eff828
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-24 10:30:47 -0600

    Complete show page tests

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,33 @@
+# Story 2 - Mechanic Show Page
+#
+# As a user,
+# When I visit a mechanic show page
+# I see their name, years of experience, and the names of all rides they’re working on
+# And I only see rides that are open
+# And the rides are listed by thrill rating in descending order (most thrills first)
+
+require 'rails_helper'
+
+RSpec.describe 'mechanic show page' do
+  before :each do
+    @jim = Mechanic.create!(name: "Jim", years_experience: 17)
+
+    @tea_cups = @jim.rides.create!(name: 'The Tea Cups', thrill_rating: 2, open: true)
+    @tot = @jim.rides.create!(name: 'Tower of Terror', thrill_rating: 8, open: true)
+    @swings = @jim.rides.create!(name: 'The Swings', thrill_rating: 5, open: false)
+  end
+
+  it 'displays the mechanics name and years experience' do
+    visit "/mechanics/#{@jim.id}"
+
+    expect(page).to have_content 'Jim'
+    expect(page).to have_content 'Experience: 17 years'
+  end
+
+  it 'shows names of all rides the mechanic is working on, ordered by thrill_rating' do
+    visit "/mechanics/#{@jim.id}"
+
+    expect(@tot).to appear_before(@swings)
+    expect(@swings).to appear_before(@tea_cups)
+  end
+end
