4a11d9a6a172aabd71494965f0915089f1acdda8
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-24 09:52:51 -0600

    Build out tests for Mechanic index page

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,38 @@
+# Story 1 - Mechanic Index Page
+#
+# As a user,
+# When I visit the mechanics index page
+# I see a header saying “All Mechanics”
+# And I see a list of all mechanic’s names and their years of experience
+# And I see the average years of experience across all mechanics
+
+require 'rails_helper'
+
+Rspec.describe 'mechanic index' do
+  before :each do
+    @jim = Mechanic.create!(name: "Jim", years_experience: 17)
+    @xena = Mechanic.create!(name: "Xena", years_experience: 23)
+  end
+
+  it 'lists all mechanics names' do
+    visit '/mechanics'
+
+    expect(page).to have_content 'All Mechanics'
+    expect(page).to have_content 'Jim'
+    expect(page).to have_content 'Xena'
+  end
+
+  it "lists each mechanic\'s years of experience" do
+    visit '/mechanics'
+
+    within "div#mechanic-#{@jim.id}" do
+      expect(page).to have_content 'Experience: 17 years'
+    end
+  end
+
+  it "displays the average years expereince across all mechanics" do
+    visit '/mechanics'
+
+    expect(page).to have_content 'Average years of experience: 20'
+  end
+end
