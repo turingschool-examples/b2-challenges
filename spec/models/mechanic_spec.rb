e6ac76cb6333e0cc7cbf5f4b6011b93c5bf2879d
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-05-24 11:37:44 -0600

    Create test for thrill rating order of open rides

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -15,4 +15,15 @@ RSpec.describe Mechanic, type: :model do
       expect(Mechanic.avg_years_experience.round(1)).to eq(17.7)
     end
   end
+
+  describe '#thrill_rating_order_on_open_rides' do
+    it 'orders by thrill rating descending' do
+      @mechanic_1 = Mechanic.create!(name: 'Jamie', years_experience: 17)
+      @ride_1 = @mechanic_1.rides.create!(name: 'Free Fall', thrill_rating: 5, open: true)
+      @ride_2 = @mechanic_1.rides.create!(name: 'Tumbler', thrill_rating: 7, open: true)
+      @ride_3 = @mechanic_1.rides.create!(name: 'Spinster', thrill_rating: 10, open: false)
+
+      expect(@mechanic_1.thrill_rating_order_on_open_rides.last).to eq(@ride_1)
+    end
+  end
 end
