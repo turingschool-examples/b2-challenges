5a818b30f533ae5ec96d2c83cd19aa6c8f8bf8e1
Zach T <ztrokey@gmail.com>
2021-05-24 11:35:08 -0600

    add deafult order to ride model

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,6 +1,8 @@
 class Ride < ApplicationRecord
   belongs_to :mechanic
 
+  default_scope {order(thrill_rating: :desc)}
+
   def self.open_rides
     Ride.where.not(open: false)
   end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -4,17 +4,18 @@ RSpec.describe 'the mechanic show' do
   it 'lists the mechanics attributes' do
     mechanic_1 = Mechanic.create!(name: 'Fix it Felix Jr.', years_experience: 20)
     mechanic_2 = Mechanic.create!(name: 'Bob the Builder', years_experience: 10)
-    ride_1 = mechanic_1.rides.create!(name: 'Crazy Coaster', thrill_rating: 10, open: true)
     ride_2 = mechanic_1.rides.create!(name: 'Kiddy Carts', thrill_rating: 2, open: true)
+    ride_1 = mechanic_1.rides.create!(name: 'Crazy Coaster', thrill_rating: 10, open: true)
     ride_3 = mechanic_1.rides.create!(name: 'Tower of Doom', thrill_rating: 20, open: false)
 
     visit "/mechanics/#{mechanic_1.id}"
-    
+    # save_and_open_page
     expect(page).to have_content(mechanic_1.name)
     expect(page).to have_content(mechanic_1.years_experience)
     expect(page).to have_content('Crazy Coaster')
     expect(page).to have_content('Kiddy Carts')
     expect(page).to_not have_content('Tower of Doom')
+    expect('Crazy Coaster').to appear_before('Kiddy Carts')
   end
 end
 
