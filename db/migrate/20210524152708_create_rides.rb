86af0615f200ee44ae546b4750d979fec25876a4
Lawrence Whalen <ldwhalen@gmail.com>
2021-05-24 09:48:16 -0600

    Create many to many relationship

--- db/migrate/20210524152708_create_rides.rb
+++ db/migrate/20210524152708_create_rides.rb
@@ -4,6 +4,7 @@ class CreateRides < ActiveRecord::Migration[5.2]
       t.string :name
       t.integer :thrill_rateing
       t.boolean :open
+      t.timestamps
     end
   end
 end
---
+++ spec/models/ride_spec.rb
@@ -0,0 +1,10 @@
+# spec/models/ride.rb
+
+require "rails_helper"
+
+RSpec.describe Ride, type: :model do
+  describe "relationships" do
+    it { should have_many(:ride_mechanics) }
+    it { should have_many(:mechanics).through(:ride_mechanics) }
+  end
+end
