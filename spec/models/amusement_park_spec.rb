a96ef874bff6337f82a4768440e3bf639573c5c1
Suzie K <suziekim.dev@gmail.com>
2021-05-24 14:03:10 -0400

    Update relationships

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe AmusementPark, type: :model do
   describe 'relationships' do
-    it {should have_many :rides}
+    it {should have_many :rides, through: :mechanics}
     it {should have_many :mechanics}
   end
 end
