3c2882af63aa171614ac59b77992aa4a90a19a94
Zach T <ztrokey@gmail.com>
2021-05-24 10:56:14 -0600

    add average years experience to mechanics show page

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,3 +1,7 @@
 class Mechanic < ApplicationRecord
   has_many :rides
+
+  def self.average_experience
+    Mechanic.all.average(:years_experience).to_i
+  end
 end
--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -12,10 +12,11 @@ RSpec.describe 'the mechanic index' do
     mechanic_2 = Mechanic.create!(name: 'Bob the Builder', years_experience: 10)
 
     visit '/mechanics'
-
+    save_and_open_page
     expect(page).to have_content(mechanic_1.name)
     expect(page).to have_content(mechanic_2.name)
     expect(page).to have_content(mechanic_2.years_experience)
     expect(page).to have_content(mechanic_2.years_experience)
+    expect(page).to have_content(15)
   end
 end
