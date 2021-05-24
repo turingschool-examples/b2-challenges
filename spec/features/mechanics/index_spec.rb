1001d4b6ece087af8aada1c813ff9d7824109a62
Suzie K <suziekim.dev@gmail.com>
2021-05-24 11:41:50 -0400

    Green test lists all mechanics and attributes

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'Mechanics index page' do
   describe 'display' do
     it 'lists all mechanics and their attributes' do
       mechanic_1 = Mechanic.create!(name: "Ryan", exp_in_years: 11)
-      mechanic_1 = Mechanic.create!(name: "Bryan", exp_in_years: 5)
+      mechanic_2 = Mechanic.create!(name: "Bryan", exp_in_years: 5)
       
       visit '/mechanics'
-
+   
       expect(page).to have_content("Name")
       expect(page).to have_content("Years of Experience")
-      expect(mechanic_1).to appear_before(mechanic_2)
+      expect(mechanic_1.name).to appear_before(mechanic_2.name)
     end
   end
 
