95333f32bdfbf9ef598822beb83ac15f235b0b12
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-24 10:07:24 -0600

    Create seed data for mechanics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jim = Mechanic.create!(name: "Jim", years_experience: 17)
+xena = Mechanic.create!(name: "Xena", years_experience: 23)
