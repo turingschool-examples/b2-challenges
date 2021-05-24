f587b6e74e8eead917582bf4224f594dbcdc7466
Caleb Wittman <calebjwittman1991@gmail.com>
2021-05-24 12:15:01 -0600

    Create initial test for park show page

---
+++ spec/features/amusement_parks/show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Amusement Park Show Page' do
+  before :each do
+    @park_1 = AmusementPark.create!(name: 'Super Fun Park', admission_price: 12)
+    @park_1 = AmusementPark.create!(name: 'Not So Super Fun Park', admission_price: 10)
+
+    visit "/amusement_parks/#{@park_1.id}"
+  end
+
+  it 'has amusement park name and admission price' do
+
+    expect(page).to have_content('Super Fun Park')
+    expect(page).to have_content(12)
+  end
+end
