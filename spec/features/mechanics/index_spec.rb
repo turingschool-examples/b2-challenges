bc465ee9bc3d29ea291efa364d0a6352878e1e50
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-24 10:28:46 -0600

    Add test for link to show page

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -1,11 +1,3 @@
-# Story 2 - Mechanic Show Page
-#
-# As a user,
-# When I visit a mechanic show page
-# I see their name, years of experience, and the names of all rides they’re working on
-# And I only see rides that are open
-# And the rides are listed by thrill rating in descending order (most thrills first)
-
 require 'rails_helper'
 
 RSpec.describe 'mechanic index' do
@@ -20,6 +12,14 @@ RSpec.describe 'mechanic index' do
     expect(page).to have_content 'All Mechanics'
     expect(page).to have_content 'Jim'
     expect(page).to have_content 'Xena'
+  end
+
+  it 'links to each mechanics show page' do
+    visit '/mechanics'
+
+    click_on 'Jim'
+
+    expect(current_path).to eq "/mechanics/#{@jim.id}"
   end
 
   it "lists each mechanic\'s years of experience" do
